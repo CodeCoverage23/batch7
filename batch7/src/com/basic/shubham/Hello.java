49e28c5c76a62f0ceeff346d9dd2e967e4f57a46
sbpar <sbpar@Shubham>
2023-07-18 21:32:09 +0530

    Hello message added

---
+++ batch7/src/com/basic/shubham/Hello.java
@@ -0,0 +1,12 @@
+package com.basic.shubham;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Added by SHubham
+		System.out.println("Hello Wolrd!!");;
+	}
+
+}
