793b11f6669c164c1f5dfe394eaa709c4fc932a4
DELL <DELL@DESKTOP-B9D9MPG>
2023-07-18 22:30:37 +0530

    just

---
+++ batch7/src/HelloJavaJust.java
@@ -0,0 +1,12 @@
+
+public class HelloJavaJust {
+ public static void main(String[] args) {
+	  //addition and Multiplication
+	 int num1=10;
+	 int num2=20;
+	 int num3=30;
+	 int totalSumMultiplication =num1+num2*num3;
+	System.out.println( "ans is "+ totalSumMultiplication);
+}
+	
+}
---
+++ batch7/src/com/basicvinod/Vinod.java
@@ -0,0 +1,8 @@
+package com.basicvinod;
+
+public class Vinod {
+public static void main(String[] args) {
+	System.out.println("Hello Java just");
+}
+	
+}
