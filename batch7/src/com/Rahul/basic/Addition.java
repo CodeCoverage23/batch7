a977ea3940d9915c1a0018a336c6eeb7e4bb445e
rahul bhoyar <rahul bhoyar@LAPTOP-PBLE07VF>
2023-07-18 22:06:18 +0530

    test its working or not

---
+++ batch7/src/com/Rahul/basic/Addition.java
@@ -0,0 +1,16 @@
+package com.Rahul.basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		int c=40;
+		int d=(c-a)+b;
+		System.out.println("Answer"+ d);
+		
+		
+	}
+
+}
