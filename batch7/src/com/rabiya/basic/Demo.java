a441ca1c5b36853a4ab7f9f1b5bfdb458f11dc8a
siraj <siraj@Siraj.bbrouter>
2023-07-19 15:20:31 +0530

    Default Value Program

--- batch7/src/com/rabiya/basic/Demo.java
+++ batch7/src/com/rabiya/basic/Demo.java
@@ -2,9 +2,8 @@ package com.rabiya.basic;
 
 public class Demo {
 
-	public static void main(String[] args)
-	{
-	    System.  out.println("code coverage");
+	public static void main(String[] args) {
+		System.out.println("code coverage");
 
 	}
 
