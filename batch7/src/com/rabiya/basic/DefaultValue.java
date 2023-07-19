a6d91087116033a352462890550c82bae69b9655
siraj <siraj@Siraj.bbrouter>
2023-07-19 15:17:12 +0530

    Default value Program

---
+++ batch7/src/com/rabiya/basic/DefaultValue.java
@@ -0,0 +1,36 @@
+package com.rabiya.basic;
+
+public class DefaultValue {
+
+	
+		
+			static int a;
+			static byte b;
+			static short c;
+			static long d;
+			static float e;
+			static double f;
+			static boolean h;
+			static char i;
+			static String r;
+
+			public static void main(String[] args) {
+				System.out.println("the deafault value of int is : "+a);
+				System.out.println("the deafault value of byte is : "+b);
+				System.out.println("the deafault value of short is : "+c);
+				System.out.println("the deafault value of long is : "+d);
+				System.out.println("the deafault value of float is : "+e);
+				System.out.println("the deafault value of double is : "+f);
+				System.out.println("the deafault value of boolean is : "+h);
+				System.out.println("the deafault value of char is : "+i);
+				System.out.println("the deafault value of String is : "+r);
+			
+			
+			}
+
+		
+
+
+	}
+
+
