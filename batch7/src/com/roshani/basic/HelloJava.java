64b17fcb5a02b75b5768427fb4a3f30850824a9c
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-07-19 19:28:06 +0530

    my new java class

---
+++ batch7/src/com/roshani/basic/HelloJava.java
@@ -0,0 +1,13 @@
+package com.roshani.basic;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// 50,10
+		int num1 = 50;
+		int num2 = 20;
+		int totalsum = num1 + num2;
+		System.out.println( "sum"+totalsum);
+
+	}
+}
