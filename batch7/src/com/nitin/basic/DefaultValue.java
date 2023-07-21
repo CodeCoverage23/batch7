29e36ad33a3724b0183a3bfe4b0172aef4341f5e
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-07-21 07:15:18 +0530

    default value program

---
+++ batch7/src/com/nitin/basic/DefaultValue.java
@@ -0,0 +1,26 @@
+package com.nitin.basic;
+
+public class DefaultValue {
+	static int a;
+	static byte b;
+	static long c;
+	static short d;
+	static double e;
+	static float f;
+	static boolean g;
+	static char h;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the default value of int is :" + a);
+		System.out.println("the default value of byte is :" + b);
+		System.out.println("the default value of long is :" + c);
+		System.out.println("the default value of short is :" + d);
+		System.out.println("the default value of double is :" + e);
+		System.out.println("the default value of float is :" + f);
+		System.out.println("the default value of boolean is :" + g);
+		System.out.println("the default value of char is :" + h);
+
+	}
+
+}
