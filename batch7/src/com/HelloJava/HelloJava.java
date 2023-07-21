d7a7991c078b7a225209c1ed19f6c7a6beb3baf0
pratiksha <pratikshanaik3008@gmail.com>
2023-07-21 11:26:55 +0530

    practice basic

---
+++ batch7/src/com/HelloJava/HelloJava.java
@@ -0,0 +1,15 @@
+package com.HelloJava;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello code coverage");
+		
+		int a = 0;
+		
+		System.out.println(a);
+
+	}
+
+}
