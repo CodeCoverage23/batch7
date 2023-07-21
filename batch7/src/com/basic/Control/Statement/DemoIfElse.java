3c663cbb13ab0ac89d62775d2f27c04cb0bd85ec
priya <priya@kubernetes.docker.internal>
2023-07-21 19:23:41 +0530

    Control Statement IfElse EXample

---
+++ batch7/src/com/basic/Control/Statement/DemoIfElse.java
@@ -0,0 +1,21 @@
+package com.basic.Control.Statement;
+
+public class DemoIfElse {
+	
+ public static void main(String[] args) {
+		
+	int age =30;
+		
+	 if(age>=20){
+			
+		System.out.println("User is Eligible for Driving Licence");
+			
+	}else {
+			
+		System.out.println("User is not Eligible for Driving Licence ");
+		}
+		
+		System.out.println("Rest of the Code");
+	}
+
+}
