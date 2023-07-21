29983cb81ac58684b698d28c55c64740ee648f75
afifa <afifa@LAPTOP-GA5HHF0V>
2023-07-21 20:19:33 +0530

    IfElse

---
+++ batch7/src/com/basic/ifelse.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		int n=45;
+				
+				if (n>=0)
+				{
+				System.out.println("Number is Positive");
+		
+				}else
+					System.out.println("Number is Negative");
+
+	}
+
+}
