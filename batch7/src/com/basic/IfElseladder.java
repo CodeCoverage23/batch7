891f3a7284943738ebc9d1fceadaade5f61c9242
afifa <afifa@LAPTOP-GA5HHF0V>
2023-07-21 20:32:36 +0530

    IfElseLadder

---
+++ batch7/src/com/basic/IfElseladder.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class IfElseladder {
+
+	public static void main(String[] args) {
+		
+		int n=0;
+		
+		if (n<0)
+		{
+			System.out.println(n + " is a Negative Number");
+		}else if (n>0)
+		{
+			System.out.println(n + " is a positive Number");
+		}else 
+		{
+			System.out.println("number is zero number");
+		}
+		
+		
+	}
+
+}
