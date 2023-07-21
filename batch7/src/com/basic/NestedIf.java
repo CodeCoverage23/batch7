7f5175d406b18a0416ece622fa895696512835e2
afifa <afifa@LAPTOP-GA5HHF0V>
2023-07-21 23:10:21 +0530

    Nested If

---
+++ batch7/src/com/basic/NestedIf.java
@@ -0,0 +1,31 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class NestedIf {
+
+	public static void main(String[] args) {
+		
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("enter your age:");
+		int age = input.nextInt() ;
+		System.out.println("enter your weight:");
+		int weight = input.nextInt();
+		System.out.println("enter your hemoglobin:");
+		double hemoglobin = input.nextInt();
+	
+
+		if (age >= 18)
+
+			//System.out.println("Now user can check weight");
+		if (weight >= 50) {
+			//System.out.println("now user can check hemoglobin");
+			
+			if (hemoglobin >= 10) {
+				System.out.println("user is fit to donate blood");
+		}
+	}
+
+}
+}
