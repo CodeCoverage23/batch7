8f3b07e153d7bc2a451908b6499d0c12e1b6a320
Anuradha sampte <anuradhasampte64@gmail.com>
2023-07-22 20:05:13 +0530

    Evenodd demo updated

---
+++ batch7/Demo1/src/com/java/addition.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class addition
+{
+	public static void main (String args[])
+	{
+		int num1= 4;
+		int num2= 5;
+		int ans = num1 + num2;
+		
+		System.out.println("num1 = " + num1 + "   num2 = " + num2 );
+		System.out.println("answer = " + ans);
+		
+		System.out.println("Addition of num1+ num2 = " + ans);
+		System.out.println(num1 + " + " + num2 + " = " + ans);
+	}
+}
