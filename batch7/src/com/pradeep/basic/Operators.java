dda5e82350077490f4956c62cd99b37aa5560d59
prade <prade@DESKTOP-1HGF01U>
2023-07-22 08:31:50 +0530

    operators

--- batch7/src/com/pradeep/basic/Operators.java
+++ batch7/src/com/pradeep/basic/Operators.java
@@ -36,23 +36,53 @@ public class Operators {
 //	  System.out.println(a<b | a>b);//true
 //	  System.out.println(a>c | a<b);//false
 		
-	//Logical operator
-	  //AND operator(&&)
-	 //1. if the first condition is true then it will check the second condition
-		System.out.println(a<c && a<++b);//true && false=false
-		System.out.println(b);
-  //2. if the first condition is false then it will not check the second condition
-		System.out.println(a>c && ++a>b);//false=false
-		System.out.println(a);
+//	//Logical operator
+//	  //AND operator(&&)
+//	 //1. if the first condition is true then it will check the second condition
+//		System.out.println(a<c && a<++b);//true && false=false
+//		System.out.println(b);
+//  //2. if the first condition is false then it will not check the second condition
+//		System.out.println(a>c && ++a>b);//false=false
+//		System.out.println(a);
 		
-		//or operator(||)
-		
-		System.out.println(a<c || a<++c);//true=true
-		System.out.println(c);
-		System.out.println(a>c || a<++c);//false||true =true
-		System.out.println(c);
-		
-		
+//		//or operator(||)
+//		
+//		System.out.println(a<c || a<++c);//true=true
+//		System.out.println(c);
+//		System.out.println(a>c || a<++c);//false||true =true
+//		System.out.println(c);
+//		
+		//Assignment Operator
+//		
+//		int x=10;
+//		int y=15;
+//		int z=x+y;
+//		System.out.println(z);
+//		//+= operator
+//		System.out.println("addition :"+(x+=15));//x=x+15=>x=10+15=25
+//		// -= operator
+//		System.out.println("substraction :"+(x-=5));//x=x-5=>25-5=5=20
+//		//*= operator
+//		System.out.println("multiplication :"+(x*=10));//x=x*10=20*10=200
+//		//  /= operator
+//		System.out.println("division :"+(x/=10));//x=x/10=200/10=20
+//		// %= operator
+//		System.out.println("remainder :"+(x%=10));//x=x%10=20/10=0
+//		
+		// Relation operator
+//		// ==,<,>,>=,<=,!=
+//		int s=10;
+//		int k=10;
+//		int j=20;
+//		System.out.println("s>k :"+(s>k));//false
+//		System.out.println("s>=k :"+(s>=k));//true
+//		System.out.println("s<k :"+(s<k));//false
+//		System.out.println("s<=j :"+(s<=j));//true
+//		System.out.println("s==k :"+(s==k));//true
+//		System.out.println("s!=k :"+(s!=k));//false
+//		
+		// ternary operator(?)
+		System.out.println(a>b?a:b);//15
 		
 	}
 
