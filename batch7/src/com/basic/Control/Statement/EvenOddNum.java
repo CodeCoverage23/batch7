c935d1201443e7f4bed92f3f8953dc2f044bdbb5
priya <priya@kubernetes.docker.internal>
2023-07-22 14:36:02 +0530

    EvenOdd Number Example

---
+++ batch7/src/com/basic/Control/Statement/EvenOddNum.java
@@ -0,0 +1,19 @@
+package com.basic.Control.Statement;
+
+public class EvenOddNum {
+	
+	public static void main(String[] args) {
+		
+		int num = 44;
+		 
+        if (num % 2 == 0) {
+ 
+            System.out.println("Entered Number is Even");
+        }else {
+ 
+             System.out.println("Entered Number is Odd");
+        }
+            System.out.println("Rest of the Code");
+    }
+
+}
