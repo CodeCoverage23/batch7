fec14587d22609a7ac01a64c0f73b35abb88c836
pratiksha <pratikshanaik3008@gmail.com>
2023-07-22 14:26:08 +0530

    task1 to find odd or even number

---
+++ batch7/src/com/Task/TaskOddEven.java
@@ -0,0 +1,22 @@
+package com.Task;
+
+import java.util.Scanner;
+
+public class TaskOddEven {
+
+	public static void main(String[] args) {
+	//program to find out number is odd or even
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter the number : ");
+		int num = s.nextInt();
+				
+		if(num%2==0) {
+			System.out.println(num + " is an even number");
+		}
+		else {
+			System.out.println(num + " is an odd number");
+		}
+
+	}
+
+}
