e5b00151b495b7e4c0b19a714ce45cb38a467add
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-07-23 20:55:57 +0530

    even odd program by using if else statement

---
+++ batch7/src/com/nitin/basic/IfElseIf.java
@@ -0,0 +1,16 @@
+package com.nitin.basic;
+
+public class IfElseIf {
+	public static void main(String[] args) {
+		int number =1;
+		if(number > 0) {
+			System.out.println("Positive Number");
+			} else if (number <0 ) {
+			System.out.println("Number is Negative");	
+			}else {
+				System.out.println("Number is Zero");
+			}
+		System.out.println(" Rest of the Part");
+	}
+
+}
