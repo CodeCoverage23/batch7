8d05e0a0e573743600abad5def0c9f78d46b5c70
DELL <DELL@DESKTOP-B9D9MPG>
2023-07-23 20:51:54 +0530

    blood criteria java program

---
+++ batch7/src/com/basicvinod/BloodCriteria.java
@@ -0,0 +1,23 @@
+package com.basicvinod;
+
+public class BloodCriteria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 21;
+		int weight = 40;
+		if
+		( age > 20 ){
+		 System.out.println( " we are checking your age ");
+		 
+			  if (weight>35) {
+				  System.out.println("you can not donet blood");
+			  }
+			 
+			 
+		
+
+	}
+
+}
+}
