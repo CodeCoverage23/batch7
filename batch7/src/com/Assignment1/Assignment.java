9f713fc675470e8af151207fd6aa900268b13be7
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-07-23 21:45:02 +0530

    Shiv sir assignment 1

---
+++ batch7/src/com/Assignment1/Assignment.java
@@ -0,0 +1,10 @@
+package com.Assignment1;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("CODE COVERAGE");
+	}
+
+}
