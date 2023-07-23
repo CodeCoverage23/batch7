3df227ca4059e9b61ee4c66c5d01d9f55eb375be
DELL <DELL@DESKTOP-B9D9MPG>
2023-07-23 21:30:03 +0530

    shiv sir  assegment

---
+++ batch7/src/com/assegment/assegment1.java
@@ -0,0 +1,12 @@
+package com.assegment;
+
+public class assegment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Code Coverage");
+
+		
+	}
+
+}
