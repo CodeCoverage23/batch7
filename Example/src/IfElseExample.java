07587437787368dde18d9f7913ad6315baa65d0f
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-24 05:25:34 +0530

    ifElse example

---
+++ Example/src/IfElseExample.java
@@ -0,0 +1,17 @@
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Num = 20;
+		if (Num + 15 < 30) {
+
+			System.out.println("Even number");
+		} else {
+			System.out.println("Odd number");
+
+		}
+
+	}
+}
