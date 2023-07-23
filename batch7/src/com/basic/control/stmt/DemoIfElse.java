7cbfa0c177a30984a07a0b88f055995ee4f7ead4
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-07-23 20:52:33 +0530

    Demo Operator

---
+++ batch7/src/com/basic/control/stmt/DemoIfElse.java
@@ -0,0 +1,15 @@
+package com.basic.control.stmt;
+
+public class DemoIfElse {
+	public static void main(String[] args) {
+		
+		int age =12;
+		if (age >= 18) {
+			System.out.println("you can vote");
+		}else {
+			System.out.println("you can not vote");
+		}
+		System.out.println("Rest of the code");
+	}
+
+}
