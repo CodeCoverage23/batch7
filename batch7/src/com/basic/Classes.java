1cbb9a9cd43b3ccc75a8d719f56450bcb843c08c
DELL <DELL@DESKTOP-B9D9MPG>
2023-07-23 21:11:47 +0530

    class . java

---
+++ batch7/src/com/basic/Classes.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 21;
+		int weight = 40;
+		if
+		( age > 20 ){
+		 System.out.println( " we are checking your age ");
+		 
+			  if (weight>35) {
+				  System.out.println("you can not donet blood");
+			  }
+}
+	}
+}
