8a5bed2126372a3b6bf5b2052e57677ec2bc696e
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-24 14:05:07 +0530

    NewProgam

--- Example/src/com/radha/java/IfElseExample.java
+++ Example/src/com/radha/java/IfElseExample.java
@@ -1,3 +1,4 @@
+package com.radha.java;
 
 public class IfElseExample {
 
---
+++ batch7/src/com/radha/basic/Add.java
@@ -0,0 +1,11 @@
+package com.radha.basic;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+	
+
+}
