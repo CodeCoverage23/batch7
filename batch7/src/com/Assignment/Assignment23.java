14e18e0a5f0e4b53b942586f2af63d46ded950e3
priya <priya@LAPTOP-SDK59G8R>
2023-07-24 18:26:53 +0530

    Declare Local variable as x .

---
+++ batch7/src/com/Assignment/Assignment23.java
@@ -0,0 +1,14 @@
+package com.Assignment;
+
+public class Assignment23 {
+	
+	 public static void main(String[] args) {
+
+	     int x;
+	     x = 10;
+	        
+	        System.out.println("The value of x is: " + x);
+	    }
+	}
+
+
