9a7868ad116fcac821db38e8abd27c10cc3c87b7
pratiksha <pratikshanaik3008@gmail.com>
2023-07-24 14:05:20 +0530

    Assignment 2 que 1

---
+++ batch7/src/com/Assignment/Assignment2q1.java
@@ -0,0 +1,25 @@
+package com.Assignment;
+//program which declare different primitive datatypes
+public class Assignment2q1 {
+	public static void main (String args[]) {
+		int a=22;
+		byte b=123;
+		float c=2;
+		char d='A'; 
+		boolean e=true;
+		double f=6544;
+		long g=56789;
+		short h=456;
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+		System.out.println("e="+e);
+		System.out.println("f="+f);
+		System.out.println("g="+g);
+		System.out.println("h="+h);
+		
+	}
+
+}
