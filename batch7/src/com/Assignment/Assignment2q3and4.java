8acd4302a8b7c2c54ed04b9f390a99c9abf768fe
pratiksha <pratikshanaik3008@gmail.com>
2023-07-24 15:20:26 +0530

    Asssignment 2 que 3 and 4

---
+++ batch7/src/com/Assignment/Assignment2q3and4.java
@@ -0,0 +1,18 @@
+package com.Assignment;
+
+public class Assignment2q3and4 {
+
+	public static void main(String[] args) {
+		
+		// que 3 : to declare local variable as x
+		// que 4 : to declare local variable as 50
+		
+		int a = 50; //declare local variable value as 50
+		char b='X'; //declare local variable as X
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+
+	}
+
+}
