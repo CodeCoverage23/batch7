1970c296011948c8b756423ddc47b43239a4b3a6
ADMIN <ADMIN@DESKTOP-KUOT65S>
2023-07-24 22:41:12 +0530

    switch case example

---
+++ batch7/src/com/Switch/SwitchCase.java
@@ -0,0 +1,26 @@
+package com.Switch;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		int days=7;
+		switch(4) {
+		case 1: System.out.println("Sunday");
+		break;
+		case 2: System.out.println("Monday");
+		break;
+		case 3: System.out.println("Tuesday");
+		break;
+		case 4: System.out.println("Wednday");
+		break;
+		case 5: System.out.println("Thursday");
+		break;
+		case 6: System.out.println("Friday");
+		break;
+		case 7: System.out.println("Saturday");
+		break;
+		}
+		System.out.println("week days");
+	}
+
+}
