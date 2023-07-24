5857b4c0ddf7f68159ba13fe2353eac7f8898050
pratiksha <pratikshanaik3008@gmail.com>
2023-07-24 14:44:43 +0530

    Assignment 2 que 2

---
+++ batch7/src/com/Assignment/Assignment2q2.java
@@ -0,0 +1,31 @@
+package com.Assignment;
+
+import java.util.Scanner;
+
+// to print first characters of your name
+public class Assignment2q2 {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in)) {
+			System.out.println("Enter your name : ");
+			String str=s.next();
+			
+			char Firstchar = str.charAt(0);
+			System.out.println("First Character of your name : "+ Firstchar);
+			
+			//to print first five characters
+			
+			char schar = str.charAt(1);
+			char tchar = str.charAt(2);
+			char fchar = str.charAt(3);
+			char vchar = str.charAt(4);
+			
+			System.out.println("First five letters of your name : "+ Firstchar+schar+tchar+fchar+vchar);
+		}
+
+
+    
+	}
+
+}
