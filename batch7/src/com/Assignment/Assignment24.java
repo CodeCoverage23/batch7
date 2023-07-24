b670c5416a7ad294adce860b58f771e33cab5a75
priya <priya@LAPTOP-SDK59G8R>
2023-07-24 18:36:37 +0530

    Assign the value of Local variable is 50

---
+++ batch7/src/com/Assignment/Assignment24.java
@@ -0,0 +1,15 @@
+package com.Assignment;
+
+public class Assignment24 {
+	
+	public static void main(String args[]){
+		
+		int a=50;
+		
+		System.out.println("The value of the a is " + a);
+		
+		
+		
+	}
+
+}
