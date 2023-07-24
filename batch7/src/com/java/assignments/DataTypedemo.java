1bdf8b0eca6465b8999da6ee2efa09f4fba97832
Anuradha sampte <anuradhasampte64@gmail.com>
2023-07-24 09:13:27 +0530

    DataType

---
+++ batch7/src/com/java/assignments/DataTypedemo.java
@@ -0,0 +1,12 @@
+package com.java.assignments;
+
+public class DataTypedemo 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello from demo...");
+	}
+
+}
