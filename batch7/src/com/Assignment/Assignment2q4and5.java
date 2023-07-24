dec63dd9e31d6d44bd40db2cd30ff14ddb769049
pratiksha <pratikshanaik3008@gmail.com>
2023-07-24 15:33:44 +0530

    Assignment 2 que 4 and 5

---
+++ batch7/src/com/Assignment/Assignment2q4and5.java
@@ -0,0 +1,15 @@
+package com.Assignment;
+
+public class Assignment2q4and5 {
+	
+	static int a=25; // declare global variable value as 25
+	static char b='Y'; //declare global variable as y
+
+	public static void main(String[] args) {
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
