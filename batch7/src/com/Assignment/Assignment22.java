383382e26c234c900273222fcda833a9be45674f
priya <priya@LAPTOP-SDK59G8R>
2023-07-24 19:02:58 +0530

    Print First Character Of Your Name.

---
+++ batch7/src/com/Assignment/Assignment22.java
@@ -0,0 +1,15 @@
+package com.Assignment;
+
+public class Assignment22 {
+	
+		 public static void main(String args[]) {
+	    	
+	        String name = "Priya";
+	        
+	        char firstCharacter = name.charAt(0);
+	        
+	        System.out.println("The first character of my name is: " + firstCharacter);
+	    }
+	}
+
+
