867529b49c7cbc97c6149162931ab049b4a88cd5
rahul bhoyar <rahul bhoyar@LAPTOP-PBLE07VF>
2023-07-24 19:38:57 +0530

    Assignment 2.5 & 2.6

---
+++ batch7/src/com/assignment/Assignment_2_5.java
@@ -0,0 +1,11 @@
+package com.assignment;
+//Write the Java Program to declare the global variables as y
+
+
+public class Assignment_2_5 {
+static int y;
+public static void main(String[] args) {
+	System.out.println(y);
+}
+
+}
---
+++ batch7/src/com/assignment/Assignment_2_6.java
@@ -0,0 +1,9 @@
+package com.assignment;
+//Write the java program to print the global variable value as 25.
+
+public class Assignment_2_6 {
+static int x=25;
+public static void main(String[] args) {
+	System.out.println("global variable value is "+  x);
+}
+}
