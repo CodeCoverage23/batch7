6f287058108c29ec76313fc184aad9f9d073676d
swati <swati@hp>
2023-07-25 15:03:09 +0530

    global variables

---
+++ batch7/src/com/assignment/Assignment2Que.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+public class Assignment2Que {
+	//global variables
+	int a=25; //global variable as 25
+	public static void main(String args[]) {
+	Assignment2Que d = new Assignment2Que();
+	int i= d.a;
+	System.out.println(i);
+	System.out.println(d.a);
+	
+	}
+}
