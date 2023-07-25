06fd2350e7c9b30769d975d55a869c8bd986a12a
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-25 15:21:22 +0530

    Assignment

---
+++ Example/src/com/radha/java/Assignment22.java
@@ -0,0 +1,18 @@
+package com.radha.java;
+
+public class Assignment22 {
+	
+	//Radha name
+	
+	static char r;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+
+
+		
+		System.out.println("r");
+
+	}
+
+}
---
+++ Example/src/com/radha/java/Assignment23.java
@@ -0,0 +1,15 @@
+package com.radha.java;
+
+public class Assignment23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		int a=50;// local variable
+		char x;
+		
+		System.out.println("a="+50);
+		System.out.println("x");
+	}
+
+}
