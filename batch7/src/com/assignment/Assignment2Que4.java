a371d6f327cb15040c16998e697bce669a7449cc
swati <swati@hp>
2023-07-25 14:55:34 +0530

    global variable

---
+++ batch7/src/com/assignment/Assignment2Que4.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class Assignment2Que4 {
+	char y;
+	public static void main(String args[]) {
+		Assignment2Que4 s = new Assignment2Que4();
+		char c =s.y;
+		
+	}
+
+}
