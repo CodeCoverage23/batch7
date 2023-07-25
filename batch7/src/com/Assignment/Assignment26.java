5c4d83fa0bd38e3b58053f56b38342beb71e8cdf
priya <priya@host.docker.internal>
2023-07-25 12:35:43 +0530

    Globle Variable value 25

---
+++ batch7/src/com/Assignment/Assignment26.java
@@ -0,0 +1,21 @@
+//Write the java program to print the global variable value as 25.
+
+package com.Assignment;
+
+public class Assignment26 {
+		
+		int a =25;
+		char b;
+		
+		
+		public static void main(String args[]) {
+			
+			Assignment26 s= new Assignment26();
+			
+			int i=s.a;
+		
+		
+		System.out.println("Globle variable :" +i);
+		}
+	}
+
