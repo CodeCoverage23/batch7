55f29c50f1a0e775a82c56bd8ae9f0d81516ef13
swati <swati@hp>
2023-07-25 14:06:34 +0530

    Local Variables

---
+++ batch7/src/com/assignment/Assign2Que3.java
@@ -0,0 +1,15 @@
+package com.assignment;
+
+public class Assign2Que3 {
+//local variable as x
+	//Assign local variable value 50
+	public static void main(String args[]) {
+		int i = 50;
+		char c= 'X';
+		System.out.println(i);
+		System.out.println(c);
+		
+		
+		
+	}
+}
