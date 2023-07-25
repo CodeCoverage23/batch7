1507069a950bf3f74b018d4157841a2b5aef7eae
priya <priya@host.docker.internal>
2023-07-25 13:11:21 +0530

    Declare  different Primitives Data types

---
+++ batch7/src/com/Assignment/Assignment21.java
@@ -0,0 +1,28 @@
+// Write the Java Program in which declares the different primitives data types.
+package com.Assignment;
+
+public class Assignment21 {
+	
+	public static void main (String args[]) {
+		
+		int     a =20;
+		float   b =22f;
+		char    c ='P';
+	    boolean d =true;
+		byte    e =123;
+		double  f =2.30;
+		long    g =2500;
+		short   h =250;
+		
+		System.out.println("a :" +a);
+		System.out.println("b :" +b);
+		System.out.println("c :" +c);
+		System.out.println("d :" +d);
+		System.out.println("e :" +e);
+		System.out.println("f :" +f);
+		System.out.println("g :" +g);
+		System.out.println("h :" +h);
+		
+		
+}
+	}
