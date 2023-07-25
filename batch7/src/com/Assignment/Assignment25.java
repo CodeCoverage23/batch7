eb0529b77c11f6838f8732a192a02f8db35dced1
priya <priya@host.docker.internal>
2023-07-25 12:42:30 +0530

    Declare Global Variable as y

---
+++ batch7/src/com/Assignment/Assignment25.java
@@ -0,0 +1,15 @@
+//Write the Java Program to declare the global variables as y
+
+package com.Assignment;
+
+public class Assignment25 {
+	
+	
+	int y ;
+	
+	public static void main(String args[]) {
+		
+	System.out.println("Globle variable as : y");
+	
+	}
+}
