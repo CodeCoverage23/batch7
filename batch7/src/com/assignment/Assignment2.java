5774fbdd23da0fab9bdfb525c8e1077255011755
swati <swati@hp>
2023-07-25 14:04:14 +0530

    PREMETIVE DATA TYPES

---
+++ batch7/src/com/assignment/Assignment2.java
@@ -0,0 +1,25 @@
+package com.assignment;
+
+public class Assignment2 {
+//different pemetive data types
+public static void main(String args[]) {
+	int a=11;
+	char c ='c';
+	float f=2/3;
+	double d=22.33;
+	long l=54678;
+	short s=245;
+	byte b=10;
+	boolean h=true;
+	System.out.println( a);
+	System.out.println(c);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(l);
+	System.out.println(s);
+	System.out.println(b);
+	System.out.println(h);
+			
+}   
+
+}
