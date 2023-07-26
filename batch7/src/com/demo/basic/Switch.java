556623f50487c881e7e17edb7d31f97127b48072
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-26 16:03:55 +0530

    switch case demo

---
+++ batch7/src/com/demo/basic/Switch.java
@@ -0,0 +1,27 @@
+package com.demo.basic;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		int number=4;
+		String day="weansday";
+	switch(number) {
+
+	case 1: System.out.println("sunday");
+	break;
+
+	case 2: System.out.println("monday");
+	break;
+
+	case 3: System.out.println("thuesady") ;
+	break;
+
+	case 4:  System.out.println("weansday");
+	break;
+
+	case 5: System.out.println("thrusday");
+	break;
+
+	}
+	}
+}
