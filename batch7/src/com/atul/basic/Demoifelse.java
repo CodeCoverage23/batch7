06aac411fa631e18667c81ebd656293e18b136ca
atulmisal2001 <139397777+atulmisal2001@users.noreply.github.com>
2023-07-26 14:05:24 +0530

    demo if else

---
+++ batch7/src/com/atul/basic/Demoifelse.java
@@ -0,0 +1,15 @@
+package com.atul.basic;
+
+public class Demoifelse {
+
+	public static void main(String[] args) {
+		int number = 25;
+		if (number < 35) {
+			System.out.println("your are fail");
+		} else if (number > 35) {
+			System.out.println("your are pass");
+		}
+
+	}
+
+}
