1e22d8f9c762bf2156d0ff8be0795d1fcded1998
prade <prade@DESKTOP-1HGF01U>
2023-07-26 08:05:10 +0530

    switch statement

---
+++ batch7/src/com/pradeep/basic/Switch.java
@@ -0,0 +1,41 @@
+package com.pradeep.basic;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+	
+	
+	int number=7;
+	String month="july";
+switch(number) {
+
+case 1: System.out.println(month);
+break;
+
+case 2: System.out.println("january");
+break;
+
+case 3: System.out.println("january") ;
+break;
+
+case 4:  System.out.println("january");
+break;
+
+case 5: System.out.println("january");
+break;
+
+case 6 : System.out.println("january");
+
+break;
+case 7: System.out.println(month);
+case 8:  System.out.println("january");
+
+case 9: System.out.println("january");
+break;
+
+default : System.out.println("inavalid");
+}
+
+//System.out.println(monthstring);
+}}
