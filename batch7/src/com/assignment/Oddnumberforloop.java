722fefdb26030e689499154d16fd56b9f6889371
swati <swati@hp>
2023-07-27 21:15:06 +0530

    odd number

---
+++ batch7/src/com/assignment/Oddnumberforloop.java
@@ -0,0 +1,19 @@
+package com.assignment;
+
+public class Oddnumberforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=100;
+		System.out.println("list of odd number from 1 to  ");
+		for(int i=1;i<=100;i++) 
+		{
+			if(i%2!=0) {
+				
+			System.out.println(i+"");
+			
+		}
+
+	}
+	}
+}
