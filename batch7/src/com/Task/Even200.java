9a82c1ca1ad5975ece5e5664161cfb0d5f90cb51
pratiksha <pratikshanaik3008@gmail.com>
2023-07-27 06:47:42 +0530

    even 200

---
+++ batch7/src/com/Task/Even200.java
@@ -0,0 +1,25 @@
+package com.Task;
+
+public class Even200 {
+	
+	//to print all even no from 1 to 200
+	
+	public static void main(String[] args) {
+		
+		int i=200;
+		
+		System.out.println("Even numbers from 1 to 200 :");
+		
+		for(i=1;i<=200;i++) {
+			
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+		
+	}
+	
+
+
+}
