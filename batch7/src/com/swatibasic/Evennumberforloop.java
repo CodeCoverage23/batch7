a0e7288f856f503dc817ec229d80a0c52fdc5bc3
swati <swati@hp>
2023-07-27 21:15:30 +0530

    even number

---
+++ batch7/src/com/swatibasic/Evennumberforloop.java
@@ -0,0 +1,24 @@
+package com.swatibasic;
+
+public class Evennumberforloop {
+	public static void main(String[] args) {
+
+			int number=100;
+			
+			System.out.print("list of even number from 1 to 100"  );
+			
+			for(int i= 1;i<=number; i++)
+			{
+				if(i%2==0)
+				{
+				System.out.print(i+" ");
+				
+			}
+					
+				
+						
+			}
+
+		}
+
+	} 
