46db7bfc27f4886454d084ad9681d839dae5e68a
pratiksha <pratikshanaik3008@gmail.com>
2023-07-27 06:47:27 +0530

    odd 200

---
+++ batch7/src/com/Task/odd200.java
@@ -0,0 +1,21 @@
+package com.Task;
+
+public class odd200 {
+
+	public static void main(String[] args) {
+		
+		
+	int i=200;
+		
+		System.out.println("Even numbers from 1 to 200 :");
+		
+		for(i=1;i<=200;i++) {
+			
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
