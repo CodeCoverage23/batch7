82d8aefdab760f6f6cd6c2ced89a11c069ecbdeb
priya <priya@LAPTOP-SDK59G8R>
2023-07-28 13:50:24 +0530

    Assignment 3:Return Student name and Print it.

---
+++ batch7/src/com/Assignment3/Question1.java
@@ -0,0 +1,26 @@
+// Design the separate method as String getStudentName (String name) which return student name and print it.
+
+package com.Assignment3;
+
+public class Question1 {
+	
+ public String getStudentName(String Name) {
+		
+		return Name;
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		Question1 s = new Question1();
+	
+	    String a=s.getStudentName("PRIYA");
+	
+	    System.out.println(a);
+	
+	
+	}
+}
+
+
+
