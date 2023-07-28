b01eee0bd2d0cd823e2d2b5be792dd8f1a137e65
ADMIN <ADMIN@DESKTOP-KUOT65S>
2023-07-28 13:47:33 +0530

    Assignment number 3

---
+++ batch7/src/com/Assignment3/AssignmentPractice.java
@@ -0,0 +1,20 @@
+package com.Assignment3;
+//Design the separate method as String getStudentName (String name) which return student name and print it.
+
+         
+          
+               public class AssignmentPractice{ 
+               
+           public String getStudentName (String name) {
+                 return name;
+                     }
+
+
+	public static void main(String[] args) {
+		AssignmentPractice l=new AssignmentPractice();
+	String b	=l.getStudentName ("Lovekesh");
+		System.out.println(b);
+	}
+	
+
+}
