fba8ab4dd0d6d0e8d64e069361a55d27cb7a1e38
Admin <Admin@LAPTOP-27656PRT>
2023-07-28 18:26:10 +0530

    Assignment 3

---
+++ batch7/src/com/Assignment3/Assignment3Que1.java
@@ -0,0 +1,17 @@
+package com.Assignment3;
+//Design the separate method as String getStudentName (String name) which return student name and print it.
+public class Assignment3Que1 {
+
+	public String getStudentName(String Name){
+		return Name;
+		
+	}
+	public static void main(String[]args) {
+		Assignment3Que1 d= new Assignment3Que1();
+		String s=d.getStudentName("Akash");
+		System.out.println(s);
+		
+
+	}
+
+}
