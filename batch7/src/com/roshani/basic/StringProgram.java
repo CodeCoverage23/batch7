5c21168d15fd6ce17fd5b25ec35b29e6efffc355
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-07-29 17:15:12 +0530

    string programm

---
+++ batch7/src/com/roshani/basic/StringProgram.java
@@ -0,0 +1,10 @@
+package com.roshani.basic;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello this is my first program in java .");
+	}
+}
