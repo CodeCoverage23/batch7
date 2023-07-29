02d951cc08aa7f00d119433af2aadfa8babc4647
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-07-29 17:55:05 +0530

    switch case

---
+++ batch7/src/com/basic/control/stmt/SwitchCase.java
@@ -0,0 +1,17 @@
+package com.basic.control.stmt;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int month =3;
+		//switch statement of months
+		case1:
+			System.out.println("january");
+		case2:
+			System.out.println("march");
+		case3:
+			System.out.println("may");
+		
+		
+	}
+
+}
