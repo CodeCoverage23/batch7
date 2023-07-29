25e77418235e0bdb295218b1bee47b39df6e036f
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-07-29 16:58:23 +0530

    Demo statement

---
+++ batch7/src/com/basic/control/stmt/DemoStmt.java
@@ -0,0 +1,21 @@
+package com.basic.control.stmt;
+
+public class DemoStmt {
+	public static void main(String[] args) {
+		
+		int age = 20;
+		if (age >= 18) {
+		
+			System.out.println("you can drive any type of vehicle.");
+		}
+		
+			//System.out.println("Hello i am here");
+		else {
+			System.out.println("you can not drive");
+		}
+		
+		System.out.println("Hello i am here..");
+		
+	}
+
+}
