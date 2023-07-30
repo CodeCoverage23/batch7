b2f3657662e0c97d7fff1dba2564d27d7ce96883
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-30 16:56:57 +0530

    Assignment4 qn no.1,2

---
+++ batch7/src/Assignment4/Assignment41.java
@@ -0,0 +1,15 @@
+package Assignment4;
+// write the java programto design method for addition of two number which returns int result to 
+//that mathod nd result should be print into main method.
+
+public class Assignment41 {
+ 
+	public static void main(String[] args) {
+		
+		int a =30;
+		 int b= 50;
+		 int c= a+b;
+System.out.println("Addition of two number is:"+ c);
+	}
+
+}
---
+++ batch7/src/Assignment4/Assignment42.java
@@ -0,0 +1,14 @@
+package Assignment4;
+//write the java programto design method for Subtraction of two number which returns int result to 
+//that mathod nd result should be print into main method.
+public class Assignment42 {
+
+	public static void main(String[] args) {
+		int x= 40;
+		int y= 20;
+		int z= x-y;
+				
+System.out.println("Subtrataction of two number is:"+z);
+	}
+
+}
