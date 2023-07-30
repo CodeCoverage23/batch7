c37fb21bd73e6f5c75d5a52a521261d7e77177cb
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 21:53:35 +0530

    assignment22

---
+++ batch7/src/assignment/assignment22.java
@@ -0,0 +1,9 @@
+package assignment;
+//WAP declare the local variable as x.
+public class assignment22 {
+	public static void main(String[] args) {
+		int x; //variable Declaration 
+		System.out.println("x");
+	}
+
+}
