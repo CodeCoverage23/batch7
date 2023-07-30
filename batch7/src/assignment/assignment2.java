53a6d5234e262762eea83cdf88c7ca9c9d17c75f
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 20:39:36 +0530

    assingnment2

---
+++ batch7/src/assignment/assignment2.java
@@ -0,0 +1,26 @@
+package assignment; 
+//WAP in which declare the different primitives.
+public class assignment2 {
+	
+	public static void main(String[]args) {
+		//declaring the all primitives data types
+		boolean b;
+		short s;
+		byte bt;
+		int i;
+		long l;
+		float f;
+		double d;
+		char c;
+		
+		System.out.println("b");
+		System.out.println("s");
+		System.out.println("bt");
+		System.out.println("i");
+		System.out.println("l");
+		System.out.println("f");
+		System.out.println("d");
+		System.out.println("c");
+	}
+
+}
