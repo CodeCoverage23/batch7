ed626da1778a8233094e254fa390ef8f23a587f0
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 19:42:33 +0530

    assignment1

---
+++ batch7/src/assignment/assignment1.java
@@ -0,0 +1,8 @@
+package assignment;
+// Write a Java Program to print Code Coverage traning.
+public class assignment1 {
+	public static void main(String args[]) {
+		System.out.println("cord coverage traning");
+	}
+
+}
