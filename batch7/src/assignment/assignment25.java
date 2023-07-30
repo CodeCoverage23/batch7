455fdaeb3c18ea8de417b2435bb9aeeb3e4684bb
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 22:40:13 +0530

    assignment25

---
+++ batch7/src/assignment/assignment25.java
@@ -0,0 +1,9 @@
+package assignment;
+//WAP to Print the global variable value as 25.
+public class assignment25 {
+	public static void main(String[] arge) {
+		int s=25;
+		System.out.println("s = 25");
+	}
+
+}
