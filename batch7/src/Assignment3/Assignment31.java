ebbab98a7b17b7dc2457bc10a798f454a2ed1689
lenovo <lenovo@DESKTOP-DQ9GDHT>
2023-07-30 16:41:34 +0530

    Assignment 3

---
+++ batch7/src/Assignment3/Assignment31.java
@@ -0,0 +1,16 @@
+package Assignment3;
+// Design the separate method as String get student name (string name) which return student name
+//and print it.
+public class Assignment31 {
+	 String getStudentName(String name) {
+     return name;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment31 r=new Assignment31();// create object
+		r.getStudentName("radha");// calling method
+		String a=r.getStudentName("radha");
+		System.out.println(a);
+	}
+
+}
