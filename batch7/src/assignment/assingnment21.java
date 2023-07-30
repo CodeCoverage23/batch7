f5a86a2378fd0f0ec5b5ba24d52e8b3a1a5099af
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 21:28:14 +0530

    assignment21

---
+++ batch7/src/assignment/assingnment21.java
@@ -0,0 +1,11 @@
+package assignment;
+//WAP to print first character of your name on.
+public class assingnment21 {
+	public static void main(String[] args) {
+		//Name is shreysh and below we are printing first character of name
+		char ch = 's';
+		System.out.println("first character is>>"+ch);
+		
+	}
+
+}
