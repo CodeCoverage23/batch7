0734f6fb3a451a4497cec0dab20f85a38ee14791
user <user@DESKTOP-FD8J3KF>
2023-07-30 05:18:46 -0700

    Assignment 1

---
+++ batch7/src/com/assigment/Assignment1.java
@@ -0,0 +1,9 @@
+package com.assigment;
+// Write the Java program to print the Code Coverage message on console.
+public class Assignment1 {
+	
+	public static void main(String args[]) {
+		System.out.println("Code Coverge");
+	}
+
+}
