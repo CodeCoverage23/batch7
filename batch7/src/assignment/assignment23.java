1851740b7d74c529ec5119869c242ce1c5d46a44
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-07-30 22:01:46 +0530

    assinment23

---
+++ batch7/src/assignment/assignment23.java
@@ -0,0 +1,9 @@
+package assignment;
+//WAP assign the local variable value as 50.
+public class assignment23 {
+	public static void main(String[]args) {
+		int s = 50; //Initization
+		System.out.println("s=50");
+	}
+
+}
