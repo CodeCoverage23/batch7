0858ff3af2db6bed7c189e873d256b165665d966
ADMIN <ADMIN@DESKTOP-KUOT65S>
2023-07-31 08:29:09 +0530

    edit and correct it

--- batch7/src/com/Loop/Task.java
+++ batch7/src/com/Loop/Task.java
@@ -1,20 +1,25 @@
 package com.Loop;
 
-public class Task{
+import java.util.Scanner;
+
+//multiple of 5 7 and both using for loop stmt print name 
+public class Task {
 	public static void main(String[] args) {
-		for(int i = 1; i<=100; i++) {
-//			System.out.println(i);
-			if(i%5==0) {
-				System.out.println(i + " love");
-			}if(i%7==0) {
-				System.out.println(i + " raut");
-			}if(i%5==0 &  i%7==0) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a value");
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+
+			if (i % 5 == 0 && i % 7 == 0) {
 				System.out.println(i + " Lovekesh Raut");
-			}
+			} else if (i % 5 == 0) {
+				System.out.println(i + " love");
+			} else if (i % 7 == 0) {
+				System.out.println(i + " raut");
+			} else
+				System.out.println(i);
+
 		}
 	}
 }
-	
-
-
-
