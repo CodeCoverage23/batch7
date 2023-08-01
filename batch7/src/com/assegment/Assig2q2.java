42dad6a0c3d6e4ccbb9ffcad3e4c3cc47faefb01
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-01 22:00:05 +0530

    pending work no 2

---
+++ batch7/src/com/assegment/Assig2q2.java
@@ -0,0 +1,15 @@
+package com.assegment;
+
+public class Assig2q2 {
+	public static void main(String[] args) {
+
+		// que no 3 : to declaer local veriable as X
+		// que no 4 ; declare local variable as 100
+
+		int s = 20;
+		int i = 77;
+		System.out.println("s=" + s);
+		System.out.println("i=" + i);
+
+	}
+}
