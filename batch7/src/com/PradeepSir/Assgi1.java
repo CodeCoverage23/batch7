5a7707ba8069e086604532c2171e97ce9d745a04
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-01 21:53:06 +0530

    pending work

---
+++ batch7/src/com/PradeepSir/Assgi1.java
@@ -0,0 +1,26 @@
+package com.PradeepSir;
+
+//progarm which declear different primitive datatypes
+public class Assgi1 {
+	public static void main(String[] args) {
+		int a = 10;
+		byte b = 20;
+		float c = 32.4f;
+		char f = 'A';
+		boolean r = true;
+		double u = 99.89;
+		long g = 2589;
+		short v = 786;
+
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+		System.out.println("c=" + c);
+		System.out.println("f=" + f);
+		System.out.println("r=" + r);
+		System.out.println("u=" + u);
+		System.out.println("g=" + g);
+		System.out.println("v=" + v);
+
+	}
+
+}
