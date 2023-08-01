94820f84e383b550c2e538be0548fdec5a2721bc
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-01 22:05:30 +0530

    pending work no 3

---
+++ batch7/src/com/assegment/Assig3q3.java
@@ -0,0 +1,13 @@
+package com.assegment;
+
+//print to first characters of your name
+
+public class Assig3q3 {
+	public static void main(String[] args) {
+		// my name is : vinod
+
+		char g = 'v';
+		System.out.println(g);
+
+	}
+}
