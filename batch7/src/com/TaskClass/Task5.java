70bbf661cdd52a338fc4ea4656dbb49c7ca3286f
swati <swati@hp>
2023-08-01 22:06:19 +0530

    Corrected class task

--- batch7/src/com/TaskClass/Task5.java
+++ batch7/src/com/TaskClass/Task5.java
@@ -3,24 +3,27 @@ package com.TaskClass;
 public class Task5 {
 	int rollNo;
 	  String name;
-	   String clgname;
+	   static String clgname;
 	  String adress;
+	  
 public  Task5	( int r,String n, String c, String a) {
 	rollNo = r;
 	name = n;
-	clgname = c;
+	clgname  = c;
 	adress = a;
+	
 }
 public void  display() {
-	System.out.println(rollNo + " " + name + " " + clgname + " " + adress + " ");
+	
+	System.out.println(rollNo + " " + name + " " + "MGICOET" + " " + adress + " ");
 	
 }
 public static void main (String [] args) {
-	Task5 t1 =new Task5(110,"Swati","MGICOET","Khamgaon");
-	Task5 t2 =new Task5(111,"Vaishnavi","MGICOET","Nandura");
-	Task5 t3 =new Task5(112,"Priya","MGICOET","Amravti");
-	Task5 t4 =new Task5(113,"Snehal","MGICOET","Shegaon");
-	Task5 t5 =new Task5(110,"Sampada","MGICOET","Nagpur");
+	Task5 t1 =new Task5(110,"Swati"," ","Khamgaon");
+	Task5 t2 =new Task5(111,"Vaishnavi","","Nandura");
+	Task5 t3 =new Task5(112,"Priya","","Amravti");
+	Task5 t4 =new Task5(113,"Snehal","","Shegaon");
+	Task5 t5 =new Task5(110,"Sampada","","Nagpur");
 	t1.display();
 	t2.display();
 	t3.display();
