099578619a8ebf40564b057b5b3dc2f1b6255022
pratiksha <pratikshanaik3008@gmail.com>
2023-08-01 23:04:54 +0530

    task factorial of 5

---
+++ batch7/src/com/Task/TaskFactorial.java
@@ -0,0 +1,18 @@
+package com.Task;
+//WAP to print factorial of 5.
+
+public class TaskFactorial {
+
+	public static void main(String[] args) {
+		int a=5;
+		int fact=1;
+		for(int i=1;i<=a;i++) {
+			
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+
+	}
+
+}
