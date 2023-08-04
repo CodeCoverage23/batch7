58355de85452a4b942a92ea993bb23b811da888f
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-04 15:22:04 +0530

    assignment6

---
+++ batch7/src/assignment/Demo.java
@@ -0,0 +1,11 @@
+package assignment;
+// WAP to Print the global variable value as 25.
+public class Demo {
+	int y =25; // Initialization
+	
+	public static void main(String[] arge) {
+		Demo demo = new Demo();
+		System.out.println("value of global variabl is>>" + demo.y);
+	}
+
+}
