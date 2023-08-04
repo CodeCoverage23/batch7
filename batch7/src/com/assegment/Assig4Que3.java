473b0c25477559c1e52307aa729d90b3f0c2164d
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-04 22:02:28 +0530

    multiplication code mansi man Assig4Que3

---
+++ batch7/src/com/assegment/Assig4Que3.java
@@ -0,0 +1,26 @@
+package com.assegment;
+
+//Write the java program to design method for multiplication of two number which returns int value 
+//to that method and result should be print into main method.
+
+
+public class Assig4Que3 {
+
+	public int  multiplication( int v, int s) {
+		return v* s;
+		
+	}
+	
+	public static void main(String[] args) {
+		Assig4Que3 Z= new Assig4Que3();
+		int B = Z.multiplication(26,99);
+		System.out.println( "multiplication of two number is:" + B);
+		
+		
+		
+	}
+	
+	
+	
+	
+}
