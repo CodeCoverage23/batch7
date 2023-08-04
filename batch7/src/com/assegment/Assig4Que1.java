561c980bb7dee8e2c514ef58adb4e0e67250fc91
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-04 21:42:39 +0530

    mansi mam Assig 4Que1

---
+++ batch7/src/com/assegment/Assig4Que1.java
@@ -0,0 +1,16 @@
+package com.assegment;
+
+public class Assig4Que1 {
+
+	public int addition(int v, int s) {
+		return v + s;
+	}
+
+	public static void main(String[] args) {
+
+		Assig4Que1 W = new Assig4Que1();
+		int c = W.addition(50, 40);
+		System.out.println("addition of 2 number is : " + c);
+	}
+
+}
