a28c7d8a5320f4828dd1df183eb5fa8b344120ec
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-04 21:55:11 +0530

    mansi man Assig4Que2

---
+++ batch7/src/com/assegment/Assig4Que2.java
@@ -0,0 +1,17 @@
+package com.assegment;
+
+
+//Write the java program to design method for substraction of two number which returns int 
+//value to that method and result should be print into main method.
+public class Assig4Que2 {
+public int substraction ( int v, int s) {
+	 return v-s;	
+}
+public static void main(String[] args) {
+	Assig4Que2  D = new Assig4Que2 ();
+	int E =  D.substraction(100,80);
+	System.out.println( "the two number of substraction is :"+ E);
+	
+}
+	
+}
