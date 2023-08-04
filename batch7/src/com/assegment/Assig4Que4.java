e1ef9c75d492b5bba0743042becde7a68b368511
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-04 22:09:43 +0530

    division que mansi mam Assig4Que_4

---
+++ batch7/src/com/assegment/Assig4Que4.java
@@ -0,0 +1,25 @@
+package com.assegment;
+//Write the java program to design method for division of two number which returns int value 
+//to that method and result should be print into main method.
+public class Assig4Que4 {
+
+	public int division( int v , int s) {
+		
+	
+	 return v/s;
+	}
+	 public static void main(String[] args) {
+		 Assig4Que4 G = new Assig4Que4 ();
+		 int H = G.division( 220,40);
+		 System.out.println(" two number of division is :"+ H);
+		 
+		 
+		 
+		 
+	}
+	
+	
+	
+	
+	
+}
