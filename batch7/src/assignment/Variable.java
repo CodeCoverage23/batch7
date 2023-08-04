d3636564bce684f5f78aa1bd490252e9246c2417
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-04 15:13:11 +0530

    variable.java

---
+++ batch7/src/assignment/Variable.java
@@ -0,0 +1,9 @@
+package assignment;
+// WAP assign the local variable value as 50.
+public class Variable {
+	public static void main(String[]  args) {
+		int x = 50; // initialization
+		 System.out.println(x = 50);
+	}
+
+}
