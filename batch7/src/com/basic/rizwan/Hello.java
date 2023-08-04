73dd63f77b904c136561755c7f0e5d618d99039f
Rizwan Ansari <Rizwan Ansari@LAPTOP-MU2MNFNM>
2023-08-04 11:04:58 +0530

    Hello Program

---
+++ batch7/src/com/basic/rizwan/Hello.java
@@ -0,0 +1,15 @@
+package com.basic.rizwan;
+
+public class Hello {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	public static void main (String [] args) {
+		
+		System.out.println("Hi Code Coverage");
+		System.out.println("Hello Java");
+
+	}
+
+}
