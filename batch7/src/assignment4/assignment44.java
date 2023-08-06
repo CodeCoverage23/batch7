076e9597c994f4bb3df1c000d460547a4a282c5b
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-06 19:46:09 +0530

    assignment4.4

---
+++ batch7/src/assignment4/assignment44.java
@@ -0,0 +1,16 @@
+package assignment4;
+// Write the java program to design method for division of two number
+//which returns int value to that method and result should be print into main method.
+
+public class assignment44 {
+	 public int division(int a, int b) {
+		 return a/b;
+	 }
+	 public static void main(String[] arge) {
+		 assignment44 u = new assignment44();
+		 int i = u.division(67, 2);
+		 System.out.println("division of two number>>"  + i);
+	 }
+	 
+
+}
