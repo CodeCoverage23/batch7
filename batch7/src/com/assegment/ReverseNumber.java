cc8a640744e5a473f1126953f4d6b94c7e7d25b3
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-06 23:42:18 +0530

    swapnil sir assig revers number

---
+++ batch7/src/com/assegment/ReverseNumber.java
@@ -0,0 +1,16 @@
+package com.assegment;
+
+//WAP to reverse the number.
+public class ReverseNumber {
+	public static void main(String[] args) {
+		int Anumber = 123456789;
+
+		System.out.println("orignal number :" + Anumber);
+
+		for (int i = 9; i >= 1; i--) {
+
+			System.out.println("ReverseNumber:" + i);
+
+		}
+	}
+}
