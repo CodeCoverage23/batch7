3f91b75d9fccc11007c4d3bf3dd26f1c005ecc1a
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-06 19:45:12 +0530

    assignment4.2

---
+++ batch7/src/assignment4/assignment42.java
@@ -0,0 +1,58 @@
+package assignment4;
+//Write the java program to design method for substraction of two number
+//which returns int value to that method and result should be print into main method.
+
+public class assignment42 {
+	public int substraction(int a, int b) {
+		return a - b;
+	}
+	public static void main(String[] arge) {
+		
+		assignment42 t = new assignment42();
+		int w = t.substraction(34, 67);
+		
+		System.out.println("substraction of two number>>" + w);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
