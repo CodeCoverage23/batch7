d70b8a667b2ba0e6ca69d4f250e56a1434fa7428
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-07 22:35:40 +0530

    String reverse

---
+++ batch7/src/com/assegment/StringReverse.java
@@ -0,0 +1,24 @@
+package com.assegment;
+
+public class StringReverse {
+
+	 public static void main(String[] args) {
+		
+		 String name ="sister";
+		 //789456123
+		 String Rev ="";
+		 for( int i=name.length () -1; i>=0; i--) {
+			Rev=Rev+name.charAt(i);
+			
+		 }
+			
+		 System.out.println("the String Reverse: "+ Rev);
+		 
+		 
+		 
+		 
+		 
+	}}
+	
+	
+
