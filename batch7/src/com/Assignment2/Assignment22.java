7d584638cb054b5780b76e2d04c950e74caa2405
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:38:19 +0530

    Assignment no 22

---
+++ batch7/src/com/Assignment2/Assignment22.java
@@ -0,0 +1,10 @@
+package com.Assignment2;
+//Write the java program to print first character of your name on console.
+public class Assignment22 {
+
+	public static void main(String[] args) {
+ char ch ='N';
+System.out.println("First character of my name: "+ch);
+	}
+
+}
