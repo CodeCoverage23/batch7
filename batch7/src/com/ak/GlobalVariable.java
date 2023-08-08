1c03f60f0abae15f5259c7fa4d4ce6cc98522f7f
aakan <aakan@Aakanksha>
2023-08-08 18:41:48 +0530

    assignment 2)6

---
+++ batch7/src/com/ak/GlobalVariable.java
@@ -0,0 +1,12 @@
+package com.ak;
+
+public class GlobalVariable {
+static int globalvariable=25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("value of global variable is:"+ globalvariable);
+
+//assignment 2)6
+	}
+
+}
