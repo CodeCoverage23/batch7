fe401700eaca0fd2de51baaa9d64049bd2f161be
aakan <aakan@Aakanksha>
2023-08-08 18:29:05 +0530

    assignment 2)2

---
+++ batch7/src/com/ak/FirstCharacterPrint.java
@@ -0,0 +1,13 @@
+package com.ak;
+
+public class FirstCharacterPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='A';
+		System.out.print(ch);
+		
+		//assignment 2)2
+	}
+
+}
