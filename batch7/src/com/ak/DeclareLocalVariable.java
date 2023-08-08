9be00534d4dec925ec05d7926b80b7e5b0482b02
aakan <aakan@Aakanksha>
2023-08-08 18:38:31 +0530

    assignment 2)3

---
+++ batch7/src/com/ak/DeclareLocalVariable.java
@@ -0,0 +1,12 @@
+package com.ak;
+
+public class DeclareLocalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='x';
+		System.out.print(ch);
+		//assignment 2)3
+	}
+
+}
