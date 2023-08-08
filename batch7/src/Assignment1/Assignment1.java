214d369747d93ca5f1a98c7c8c819476fe41b047
roshni1311 <134851550+roshni1311@users.noreply.github.com>
2023-08-08 08:52:12 +0530

    Assignmnet1

---
+++ batch7/src/Assignment1/Assignment1.java
@@ -0,0 +1,10 @@
+package Assignment1;
+
+public class Assignment1 {
+	//Write the java program to print the code coverage message on consol.
+	public static void main(String[] args) {
+		
+		System.out.println("Code Coverage...");
+	}
+
+}
