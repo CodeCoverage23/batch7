d06091e6e0922e3249b8d8b2e98beefa41deda5f
Dell <Dell@DESKTOP-1L19HP1>
2023-08-08 14:54:18 +0530

    new chnange

--- batch7/src/com/assignment/Assignment1.java
+++ batch7/src/com/assignment/Assignment1.java
@@ -3,6 +3,7 @@ package com.assignment;
 public class Assignment1 {
 	public static void main(String args[]) {
 		System.out.println("code coverage");
+		System.out.println("code coverage");
 	}
 
 }
