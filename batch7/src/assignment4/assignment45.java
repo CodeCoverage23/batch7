43973f8cba22d3c167859487d873d3377b9c854f
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-08 19:23:38 +0530

    assignment4.5

---
+++ batch7/src/assignment4/assignment45.java
@@ -0,0 +1,53 @@
+package assignment4;
+// Write the Java Program to calculate total of five subject marks and average of it.
+
+public class assignment45{
+	public static void main(String[] arge) {
+		int a = 45;
+		int b = 34;
+		int c = 78;
+		int d = 89;
+		int e = 67;
+		float total = a+b+c+d+e;
+		System.out.println("total of five sub " + total);
+		float avg = total/5;
+		System.out.println("avg of five sub " + avg);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
