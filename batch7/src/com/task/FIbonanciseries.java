869ec60e1199892d47027b42ec70475495e07964
siraj <siraj@Siraj>
2023-08-08 07:14:16 +0530

    Task-Fibonanci series

---
+++ batch7/src/com/task/FIbonanciseries.java
@@ -0,0 +1,27 @@
+package com.task;
+
+import java.util.Scanner;
+
+public class FIbonanciseries 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		int n01 = 0;
+		int n02 = 1;
+		System.out.print(n01 + " " + n02);
+
+		for (int i = 2; i < num; i++)
+		{
+			int next = n01 + n02;
+			System.out.print(" " + next);
+			n01 = n02;
+			n02 = next;
+
+		}
+
+	}
+}
