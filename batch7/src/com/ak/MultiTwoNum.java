9a9528b628a3561a3dca905d946a72972d16297c
aakan <aakan@Aakanksha>
2023-08-08 18:44:58 +0530

    assignment 4)3

---
+++ batch7/src/com/ak/MultiTwoNum.java
@@ -0,0 +1,17 @@
+package com.ak;
+
+public class MultiTwoNum {
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MultiTwoNum d=new MultiTwoNum();
+		int c=d.multiplication(97,72);
+		System.out.println("multiplication of two numbers: "+c);
+		
+		//assignment 4)3
+	}
+
+}
