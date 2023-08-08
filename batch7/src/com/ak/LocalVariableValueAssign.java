fea02095652f729a9cd361e0f2ee0b4cab81328d
aakan <aakan@Aakanksha>
2023-08-08 18:39:54 +0530

    assignment2)4

---
+++ batch7/src/com/ak/LocalVariableValueAssign.java
@@ -0,0 +1,13 @@
+package com.ak;
+
+public class LocalVariableValueAssign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 50;
+		System.out.println(x);
+		
+		//assignment2)4 
+	}
+
+}
