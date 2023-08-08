3b83cfe1ef8880845f70db86ce34a9569db95637
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:39:03 +0530

    Assignment no 24

---
+++ batch7/src/com/Assignment2/Assignment24.java
@@ -0,0 +1,10 @@
+package com.Assignment2;
+//Write the java program to assign local variable value as 50.
+public class Assignment24 {
+
+	public static void main(String[] args) {
+int x =50; // value declared to lacal variable
+System.out.println("value of x is :"+x);
+	}
+
+}
