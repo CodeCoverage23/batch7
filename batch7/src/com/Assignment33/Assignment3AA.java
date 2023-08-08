fcd2d1752fbfcb2bb818f0c9f58d96f5e0b8a988
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:33:50 +0530

    Assignment no 3

---
+++ batch7/src/com/Assignment33/Assignment3AA.java
@@ -0,0 +1,22 @@
+package com.Assignment33;
+//Design the separate method as String getStudentName (String name) which return student name and print it.
+
+public class Assignment3AA {
+	String getStudentName(String name) { 
+
+	return name;
+	}
+	public static void main(String[] args) {
+	 Assignment3AA N = new Assignment3AA(); 
+
+	
+
+	String s = N.getStudentName("Nitin");
+
+	System.out.println("student name:" + s);
+
+	}
+	}
+	
+
+
