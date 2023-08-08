b59fb9cb9a131c326e2f1b838aaa24f2ed53bc52
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:39:23 +0530

    Assignment no 25

---
+++ batch7/src/com/Assignment2/Assignment45.java
@@ -0,0 +1,11 @@
+package com.Assignment2;
+//Write the Java Program to declare the global variables as y
+
+public class Assignment45 {
+int y;
+
+	public static void main(String[] args) {
+
+	}
+
+}
