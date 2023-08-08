2156d7adc8efcfcea5fbb51f57883f82026c639d
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-08 08:44:57 +0530

    assig no 4 que no 5

---
+++ batch7/src/com/assegment/Assig4que5.java
@@ -0,0 +1,18 @@
+package com.assegment;
+
+public class Assig4que5 {
+	public static void main(String[] args) {
+
+		int m1 = 46;
+		int m2 = 50;
+		int m3 = 40;
+		int m4 = 80;
+		int edc = 56;
+		int Total = m1 + m2 + m3 + m4 + edc;
+		System.out.println("total sub :" + Total);
+		int avg = Total / 5;
+		System.out.println("avg 5 sub :" + avg);
+
+	}
+
+}
