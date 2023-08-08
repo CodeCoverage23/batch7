758229b336ac441486ddc0e8503814f0a8ec447b
aakan <aakan@Aakanksha>
2023-08-08 18:46:11 +0530

    assignment 4)5

---
+++ batch7/src/com/ak/Avg.java
@@ -0,0 +1,24 @@
+package com.ak;
+
+public class Avg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char DS = 76;
+		char PYTHON = 80;
+		char JAVA = 66;
+		char NETWORKING = 77;
+		char C = 88;
+		int total = DS + PYTHON + JAVA + NETWORKING + C;
+
+		System.out.println("total of subject" + total);
+
+		double avg = total / 5;
+		System.out.println("total of five subject" + avg);
+		
+		//assignment 4)5
+
+	}
+
+}
