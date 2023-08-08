5af9b860e0a9c4546da425b32877bd0f0f885115
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:38:09 +0530

    Assignment no 21

---
+++ batch7/src/com/Assignment2/Assignment21.java
@@ -0,0 +1,17 @@
+package com.Assignment2;
+//1. Write the Java Program in which declares the different primitives data types.
+public class Assignment21 {
+	public static void main(String[] args) {
+
+		 int a;
+		 byte b;
+		 long c;
+		 short d;
+		 double e;
+		 float f;
+		 boolean g;
+		 char ch;
+	
+
+	}
+}
