dc2438c76222d9ffc136f5798de5eb9d56d8d2cd
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:31:22 +0530

    Assignment no 4B

---
+++ batch7/src/com/Assignment4/Assignment4B.java
@@ -0,0 +1,19 @@
+package com.Assignment4;
+//Write the java program to design method for substraction of two number which returns int value to that method and result should be print into main method.
+
+public class Assignment4B {
+	public int sub(int a, int b) {
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		Assignment4B n=new Assignment4B();
+       int c = n.sub(30,20);
+    System.out.println("substraction of two no is:"+c);
+				
+
+	
+}
+	}
+
+
