2a53f374fc9169044494ff2eb0237ad0a3d90377
aakan <aakan@Aakanksha>
2023-08-08 18:26:05 +0530

    Assignment 2)1

---
+++ batch7/src/com/ak/PrimitivesDataTypes.java
@@ -0,0 +1,28 @@
+package com.ak;
+
+public class PrimitivesDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declaring the primitive data types
+		byte mybyte = 127;
+		short myShort = 32767;
+		int myInt = 2147483647;
+		long mylong = 95522554454552554L;
+		float myFloat = 1.25544554f;
+		double myDouble = 1.22555554454744d;
+		boolean myBoolean = true;
+		char myChar = 'A';
+		
+		System.out.print(myBoolean);
+		System.out.print(myChar);
+		System.out.print(myDouble);
+		System.out.print(myFloat);
+		System.out.print(mylong);
+		System.out.print(mybyte);
+		System.out.print(myInt);
+		System.out.print(myShort);
+		//assignment 2)1 
+	}
+
+}
