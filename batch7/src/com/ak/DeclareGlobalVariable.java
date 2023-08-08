4056d94e4f45b3b7a0b6b6fc3ce98fed3b16e6e5
aakan <aakan@Aakanksha>
2023-08-08 18:40:30 +0530

    //assignment 2)5

---
+++ batch7/src/com/ak/DeclareGlobalVariable.java
@@ -0,0 +1,12 @@
+package com.ak;
+
+public class DeclareGlobalVariable {
+	char ch='y'; //global variable declaration
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='y';
+		System.out.print(ch);
+		//assignment 2)5
+	}
+
+}
