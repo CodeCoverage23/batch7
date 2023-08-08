71372a5e9c1c4ffd71656cdfc181df9d5f51ac30
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:39:40 +0530

    Assignment no 26

---
+++ batch7/src/com/Assignment2/Assignment46.java
@@ -0,0 +1,11 @@
+package com.Assignment2;
+//Write the java program to print the global variable value as 25.
+
+public class Assignment46 {
+int y =25;
+	public static void main(String[] args) {
+		Assignment46 v =new Assignment46();
+System.out.println("the value of globle variable is :"+v.y);
+	}
+
+}
