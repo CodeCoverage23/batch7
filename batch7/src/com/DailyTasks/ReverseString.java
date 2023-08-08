6ec795cb1c6c8a31f59959d8e3a3c131060492db
priya <priya@host.docker.internal>
2023-08-08 19:47:22 +0530

    Reverse String

---
+++ batch7/src/com/DailyTasks/ReverseString.java
@@ -0,0 +1,32 @@
+package com.DailyTasks;
+
+public class ReverseString {
+	
+	    public static void main(String[] args) {
+	    	
+	      String name = "Java Programming";
+	        String rev="" ;
+	      
+	       
+	        for (int i = name.length() - 1; i >= 0; i--) {
+	        
+	       rev=rev+name.charAt(i);
+	   
+	        
+	    }
+	        
+	        System.out.println("The reverse string : "+rev);
+
+
+}
+}
+	
+
+
+	
+	    
+
+	 
+
+
+
