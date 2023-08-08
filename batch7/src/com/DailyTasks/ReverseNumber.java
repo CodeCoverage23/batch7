f0da6a722233dc37b7f1ab484d513dc8e6fc182b
priya <priya@host.docker.internal>
2023-08-08 19:46:31 +0530

    Reverse Number

---
+++ batch7/src/com/DailyTasks/ReverseNumber.java
@@ -0,0 +1,24 @@
+package com.DailyTasks;
+
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+		
+		int number = 12345;
+       
+           System.out.println("Original Number: " + number);
+      
+        for (int i = 5; i >= 1; i--) {
+			
+			System.out.print( +i);
+			
+			
+        }
+	}
+}
+
+   
+    
+
+
+
