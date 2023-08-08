77d9102b9cd6e048b5b79990ae1371a17d58e02b
aakan <aakan@Aakanksha>
2023-08-08 18:43:36 +0530

    assignment 4)1

---
+++ batch7/src/com/ak/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+package com.ak;
+
+public class AddTwoNumbers {
+	public static int add(int num1,int num2) {
+		int sum =num1 + num2;
+		return sum;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int num1=10;
+			int num2=20;
+			
+			int result = add(num1, num2);
+			
+			System.out.println("the sum of "+ num1+ "and " + num2+ "is: "+ result);
+			
+			//assignment 4)1
+			}
+
+}
