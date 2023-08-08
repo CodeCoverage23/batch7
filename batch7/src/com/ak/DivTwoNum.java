25bad92d10c0b5509803a05e8b1803b08548a5af
aakan <aakan@Aakanksha>
2023-08-08 18:45:43 +0530

    assignment 4)4

---
+++ batch7/src/com/ak/DivTwoNum.java
@@ -0,0 +1,17 @@
+package com.ak;
+
+public class DivTwoNum {
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DivTwoNum d=new DivTwoNum();
+		int c=d.division(972, 9);
+		System.out.println("division of two numbers:" +c);
+		
+		//assignment 4)4
+	}
+
+}
