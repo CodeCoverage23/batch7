44c963ba812d8c15865ad2c9345d0f6bf9f57d39
DELL <DELL@DESKTOP-B9D9MPG>
2023-08-08 09:06:46 +0530

    Assig no 3 que no 1

---
+++ batch7/src/com/assegment3/Assig3que1.java
@@ -0,0 +1,24 @@
+package com.assegment3;
+//Design the separate method as String getStudentName (String name)
+//which return student name and print it.
+public class Assig3que1 {
+
+	String getStudentName(String name ) {
+		
+		return name;
+		
+	}
+	public static void main(String[] args) {
+		
+		Assig3que1 A= new 	Assig3que1();
+		
+		String S = A.getStudentName("Kshitij") ;
+		
+		System.out.println( "student name :"+ S);
+		
+	}
+	
+	
+	
+	
+}
