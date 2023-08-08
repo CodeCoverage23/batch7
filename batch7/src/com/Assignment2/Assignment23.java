13d346ca6b224a73c9fedd79ea006b07853d93af
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:38:46 +0530

    Assignment no 23

---
+++ batch7/src/com/Assignment2/Assignment23.java
@@ -0,0 +1,7 @@
+package com.Assignment2;
+// Write the Java Program to declare the local variable as x .
+public class Assignment23 {
+	public static void main(String [] args) {
+int x;// local veriable
+
+	}}
