90ff9e22101ed9495f464e78e15c180c9bd2ffc4
aakan <aakan@Aakanksha>
2023-08-08 18:44:17 +0530

    assignment 4)2

---
+++ batch7/src/com/ak/SubtractTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.ak;
+
+public class SubtractTwoNumbers {
+	public int sub(int a,int b)
+	{
+		return a-b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SubtractTwoNumbers d=new SubtractTwoNumbers();
+		int c=d.sub(97, 72);
+		System.out.println("subtraction of two numbers:" + c);
+		
+		//assignment 4)2
+	}
+
+}
