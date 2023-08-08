013b62b925f7d2131c8dc458583b531303f72cad
Nitin Dhunde <Nitin Dhunde@Nitin>
2023-08-08 11:32:11 +0530

    Assignment no 4E

---
+++ batch7/src/com/Assignment4/Assignment4E.java
@@ -0,0 +1,18 @@
+package com.Assignment4;
+//Write the Java Program to calculate total of five subject marks and average of it.
+
+public class Assignment4E {
+
+	public static void main(String[] args) {
+int a =50; // Marks of English
+int b =55; // Marks of Math
+int c =60; // Marks of Hindi
+int d =65; // Marks of Marathi
+int e =70; // Marks of Science
+int total = a+b+c+d+e;
+System.out.println("Total of five subject :"+total);
+double avg = total/5;
+System.out.println("Average of five subject"+ ":"+avg);
+	}
+
+}
