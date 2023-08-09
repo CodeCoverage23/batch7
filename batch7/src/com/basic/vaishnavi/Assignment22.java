622b92f4994d1fb8b7f515c798a62c5b2c469b6d
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-09 16:29:09 +0530

    Assignment22

---
+++ batch7/src/com/basic/vaishnavi/Assignment22.java
@@ -0,0 +1,10 @@
+package com.basic.vaishnavi;
+
+public class Assignment22 {
+	
+	public static void main(String[] args) {
+		char ch = 'V';
+		System.out.println("First character is>>"+ch);
+	}
+
+}
