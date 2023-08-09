654240f0721fe5532587c28c96c6eeb141c6476f
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-09 16:20:59 +0530

    Assignment21

---
+++ batch7/src/com/basic/vaishnavi/Assignment21.java
@@ -0,0 +1,17 @@
+package com.basic.vaishnavi;
+
+public class Assignment21 {
+	public static void main(String[] args) {
+		
+	int i;
+	byte b;
+	short s;
+	long l;
+	float f;
+	double d;
+	char c;
+	boolean bl;
+	
+	}
+
+}
