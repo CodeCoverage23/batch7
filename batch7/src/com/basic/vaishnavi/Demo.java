98a92e0e22839a6057c16f13ece8cfec1a0adfe8
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-09 16:50:21 +0530

    Assignment 26

---
+++ batch7/src/com/basic/vaishnavi/Demo.java
@@ -0,0 +1,13 @@
+package com.basic.vaishnavi;
+// Assignment 26
+public class Demo {
+	
+	int y = 25;
+	public static void main(String[] args) {
+		
+		Demo demo = new Demo();
+		
+		System.out.println("The value of global variable is>>"+demo.y);
+	}
+
+}
