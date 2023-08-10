83470ed914dbe621e3757a98175f001aff6ead53
aakan <aakan@Aakanksha>
2023-08-10 20:18:49 +0530

    Assignment 5)7

---
+++ batch7/src/com/ak/SquareNumber.java
@@ -0,0 +1,21 @@
+package com.ak;
+
+import java.util.Scanner;
+
+public class SquareNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter The Number: ");
+		double number = input.nextDouble();
+		
+		double square = number*number;
+		System.out.println("Sqaure of Given Number is: " + square);
+		
+	}
+	//Assignment 5)7
+}
+
+
