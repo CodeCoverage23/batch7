792e3364f00256502e8cbb3f01269e615b1451b8
swati <swati@hp>
2023-08-10 12:51:45 +0530

    Assignment 5

---
+++ batch7/src/com/Assign5/Que3.java
@@ -0,0 +1,20 @@
+package com.Assign5;
+
+public class Que3 {
+	public static void main(String args[]) {
+		int a = 10, b = 40, c = 65;
+		if (a > b) {
+			if (a > c)
+				System.out.print(a);
+			else
+				System.out.print(c);
+		} else {
+			if (b > c)
+
+				System.out.print(b);
+			else
+				System.out.print(c);
+
+		}
+	}
+}
