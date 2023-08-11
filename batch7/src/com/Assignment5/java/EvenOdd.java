72b1ac629ee79af11f405e16e0610bebf2575794
priya <priya@LAPTOP-SDK59G8R>
2023-08-11 19:34:02 +0530

    Assignment 5 : Even Odd Number

---
+++ batch7/src/com/Assignment5/java/EvenOdd.java
@@ -0,0 +1,22 @@
+//5. Write a program to check whether number is even or odd.
+
+
+package com.Assignment5.java;
+
+public class EvenOdd {
+	
+public static void main(String[] args) {
+		
+		int num = 57;
+		 
+        if (num % 2 == 0) {
+ 
+            System.out.println("Entered Number is Even");
+        }else {
+ 
+             System.out.println("Entered Number is Odd");
+        }
+            System.out.println("Rest of the Code");
+    }
+
+}
