408a400553c774bcf2f4bcd04d5e5bd00e778e3a
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-11 20:00:41 +0530

    assignment5.4

---
+++ batch7/src/assignment5/assignment54.java
@@ -0,0 +1,23 @@
+package assignment5;
+
+import java.util.Scanner;
+
+// Write the program to check whether the no is greater than 100.
+
+public class assignment54 {
+	public static void main(String[] arge) {
+		Scanner Sc= new Scanner(System.in);
+		 System.out.println("enter a number");
+		 int num=Sc.nextInt();
+		 if(num>100)
+		 {
+			 System.out.println(" that number is bigger :" +num);
+		 }
+		 else
+		 {
+			 System.out.println("that number is smaller :" +num);
+		 }
+		
+	
+	}
+}
