128068fbffea77a34c222e33b27ed8018cfa900a
priya <priya@LAPTOP-SDK59G8R>
2023-08-11 19:32:42 +0530

    Assignment 5 : Find Maximum Number

---
+++ batch7/src/com/Assignment5/java/MaximumNumber.java
@@ -0,0 +1,27 @@
+//3.write a program to find out maximum number among three number
+
+package com.Assignment5.java;
+
+public class MaximumNumber {
+	
+	public static void main(String args[]) {
+		
+	 int a=40;
+	 int b=60;
+	 int c=30;
+
+	 if(a>b) {
+		 
+		 System.out.println("The number of a is Maximum Number");
+	 }
+	 else if (b>c) {
+		 
+		 System.out.println("The Number of b is Maximum Number");
+	 }
+	 else{
+		 
+		 System.out.println("The Number of c is maximum Number");
+	 }
+
+}
+}
