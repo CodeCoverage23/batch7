5b9a024c80c7487f5411843cd775f68b4f0d0001
ADMIN <ADMIN@DESKTOP-KUOT65S>
2023-08-11 17:08:41 +0530

    assignment5

--- batch7/src/com/Assignment/AssignmentNo5Quen2.java
+++ batch7/src/com/Assignment/AssignmentNo5Quen2.java
@@ -10,11 +10,11 @@ public class AssignmentNo5Quen2 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a year");
 		int year = sc.nextInt();
-		if(year%4==0 || year%400==0 && year!=0) {
+		if (year % 4 == 0 || year % 400 == 0 && year != 0) {
 			System.out.println(year + " : year is leap year");
-		}else
-			System.out.println(year +" : year is not leap year");
-		
+		} else
+			System.out.println(year + " : year is not leap year");
+
 	}
 
 }
