6deaebea4ae4f220873e784e65b64ef3ea39fadf
priya <priya@LAPTOP-SDK59G8R>
2023-08-11 19:30:48 +0530

    Assignment 5 : Check Number is positive & Negative

---
+++ batch7/src/com/DailyTasks/PositiveNegativeNumber.java
@@ -0,0 +1,25 @@
+// 1.write a program to check the number is positive or negative.
+
+
+
+package com.DailyTasks;
+
+public class PositiveNegativeNumber {
+	
+	public static void main(String args[]) {
+		
+	int num =50;
+	
+	 if(num>0){
+			
+		System.out.println("The Number is Possitive");
+			
+	}else {
+			
+		System.out.println("The Number is Negative ");
+		}
+		
+		System.out.println("Rest of the Part");
+	}
+
+}
