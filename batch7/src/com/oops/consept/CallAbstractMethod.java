fc1fc02d14127cf385a9481781558d5081cc3b10
swapn <swapn@LAPTOP-BJNVAM67>
2023-08-14 21:12:26 +0530

    Abstract class

---
+++ batch7/src/com/oops/consept/CallAbstractMethod.java
@@ -0,0 +1,11 @@
+package com.oops.consept;
+
+public class CallAbstractMethod {
+	public static void main(String[] args) {
+
+		AbstractClassDemo demo = new AbstractClassDemo();
+		demo.display("Code Coverage");
+		demo.printData();
+	}
+
+}
