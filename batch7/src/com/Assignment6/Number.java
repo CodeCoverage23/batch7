d0a8237ca74dee6ad25772001a2e957a7b2de7c7
priya <priya@host.docker.internal>
2023-08-14 23:58:39 +0530

    Assignment 6 -Que1) Print 25 to 50 Number using While loop

---
+++ batch7/src/com/Assignment6/Number.java
@@ -0,0 +1,22 @@
+//1.write a program to print 25 to 50 numbers using while loop.
+
+package com.Assignment6;
+
+public class Number {
+	
+	public static void main(String args[]) {
+		
+		int i =25;
+		
+		while (i<=50) {
+			
+			System.out.println(i);
+			
+			
+			i++;
+		}
+	}
+	
+	
+
+}
