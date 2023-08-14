fb48fc2c76ac6deedd769f9d841b8b64bceeee55
priya <priya@host.docker.internal>
2023-08-15 00:02:15 +0530

    Assignment 6- Que 3) Print Even number from 50 to 75

---
+++ batch7/src/com/Assignment6/EvenNumber.java
@@ -0,0 +1,22 @@
+//3.write a program  to print all even number from 50 to 75.
+
+package com.Assignment6;
+
+public class EvenNumber {
+	
+	public static void main (String args[]) {
+		int i= 50; 
+		
+		for(i=50; i<=75; i++){
+			
+		if(i%2==0);
+			
+			System.out.println(i++);
+			
+			
+		}
+		
+		
+	}
+
+}
