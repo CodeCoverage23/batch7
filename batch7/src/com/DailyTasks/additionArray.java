b7eae749c2dca7432e3d51b868f100f490dcd31c
priya <priya@host.docker.internal>
2023-08-14 23:56:03 +0530

    Perform Addition of Array

---
+++ batch7/src/com/DailyTasks/additionArray.java
@@ -0,0 +1,23 @@
+// WAP to perform addition of the array.
+
+package com.DailyTasks;
+
+public class additionArray {
+	
+	    public static void main(String[] args) {
+	    	
+	        int[] numbers = { 5, 10, 15, 20, 25 }; 
+	        int sum = 0;
+
+
+	        for (int num : numbers) {
+	            sum += num;
+	        }
+
+	        System.out.println("Sum of the array elements: " + sum);
+	    }
+	}
+
+    
+
+
