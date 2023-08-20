f2196f57271709177fa8e829ead763793429087f
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-20 18:51:03 +0530

    Assignment 6.2

---
+++ batch7/src/com/assignments/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package com.assignments;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	
+	public static void main(String[] args) {
+		
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number"); 
+		num = sc.nextInt();
+		
+		for( int i = 1; i<=10; i++)
+		{
+			System.out.println(num +"*" +i+ "=" +num*i);
+		}
+		
+	}
+
+}
