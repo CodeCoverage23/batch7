a5cd71dbae5f7845e9c11fecdad1c2a230667395
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-20 18:51:34 +0530

    Assignment 6.2

---
+++ batch7/src/com/assignments/Multiplication.java
@@ -0,0 +1,19 @@
+/*Assignment 4.3 Write a program to design method for multiplication of two
+numbers which returns int value to that method & result should be printed 
+into main method*/
+
+package com.assignments;
+
+public class Multiplication {
+	
+	public static void main(String[] args) {
+		
+	
+	int n1 = 15, n2 = 15, mul;
+	mul = n1*n2;
+    System.out.println("The multiplication of two numbers is:"+mul);
+
+	
+	}
+	
+}
