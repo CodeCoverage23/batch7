045273e1c637eb4e3caff9cef0d845a0628e088e
ADMIN <ADMIN@DESKTOP-8Q05TMM>
2023-08-20 16:34:42 +0530

    Assignment 6.1

---
+++ batch7/src/com/assignments/Numbers.java
@@ -0,0 +1,24 @@
+/*Assignment 6.1 Write a program to print 25 to 50 numbers
+using while loop*/
+
+package com.assignments;
+
+public class Numbers {
+	
+	public static void main(String[] args) {
+		
+		int i = 25;
+		
+		System.out.println("Output is : ");
+		
+		while(i<=50)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+		
+	}
+	
+	}
+
