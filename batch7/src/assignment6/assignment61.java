3f85ec188ad0641694bbaeda113a3719e1f616e4
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-20 21:57:17 +0530

    assignment6.1

---
+++ batch7/src/assignment6/assignment61.java
@@ -0,0 +1,16 @@
+package assignment6;
+// .write a program to print 25 to 50 numbers using while loop.
+public class assignment61 {
+	public static void main(String[] arge) {
+	int number=25;
+	
+	do {
+		System.out.println(number);
+		number++;
+	}
+	while(number <51);
+	
+	
+}
+
+}
