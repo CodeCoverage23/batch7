9c8ee5ddd4a161cc68d1b3c74d340b22cf966b35
Dell <Dell@DESKTOP-1L19HP1>
2023-08-23 22:26:00 +0530

    Assignment61

---
+++ batch7/src/com/assignment/Assignment61.java
@@ -0,0 +1,15 @@
+package com.assignment;
+
+public class Assignment61 {
+
+	public static void main(String[] args) {
+		int i=25;
+		while(i<31) {
+			System.out.println(i);
+			i++;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
