e47edf53c635bb9b99539814298d776dcbb82ece
HP <HP@DESKTOP-P17JSJN>
2023-08-23 20:12:15 +0530

    Swap program

---
+++ batch7/src/com/Assignments/basic/Swap.java
@@ -0,0 +1,20 @@
+package com.Assignments.basic;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		
+		int a=2,b=4;
+		
+		System.out.println("number to be swaped=" + a+b);
+		
+		int temp= a;
+	     a=b;
+	     b= temp;
+	     
+	     System.out.println("swaped number=" +  "" +a+b);
+		
+
+	}
+
+}
