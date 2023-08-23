a5ce82a068a0f18284a53077f995637ccb68c2de
HP <HP@DESKTOP-P17JSJN>
2023-08-23 16:56:42 +0530

    GetDayWish

---
+++ batch7/src/com/Assignments/basic/GetDayWish.java
@@ -0,0 +1,39 @@
+package com.Assignments.basic;
+
+import java.time.LocalTime;
+
+public class GetDayWish {
+
+	public static void main(String[] args) {
+		
+		LocalTime now= LocalTime.now();
+		System.out.println(now);
+		
+		int hours= now.getHour();
+		
+		
+		if (hours>=21 && hours <=6)
+		{
+			System.out.println("Good Night");
+		} else if(hours>=6 && hours<= 12)
+		{
+			System.out.println("Good Morning");
+		} else if(hours>=12 && hours<= 4)
+			
+		{
+			System.out.println("Good Afternoon");
+		} else if(hours>=16 && hours<= 20)
+		{
+			System.out.println("Good Evening");
+		}
+		
+			
+		}
+		
+		
+		
+		
+
+
+
+}
