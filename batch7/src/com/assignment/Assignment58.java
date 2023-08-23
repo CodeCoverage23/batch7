f3f2d0658bbaee64af0a2a0949fcea61f51d9f4f
Dell <Dell@DESKTOP-1L19HP1>
2023-08-23 22:25:24 +0530

    Assignment58

---
+++ batch7/src/com/assignment/Assignment58.java
@@ -0,0 +1,18 @@
+package com.assignment;
+
+public class Assignment58 {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=40;
+		int temp;
+		System.out.println("before swapping:" + a +" "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("after swapping:" + a +" "+b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
