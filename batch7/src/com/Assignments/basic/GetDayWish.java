cc94b01c46db8525c3c126684a29590ea758c0ad
HP <HP@DESKTOP-P17JSJN>
2023-08-23 21:52:51 +0530

    changes made as per requirement.

--- batch7/src/com/Assignments/basic/GetDayWish.java
+++ batch7/src/com/Assignments/basic/GetDayWish.java
@@ -12,17 +12,17 @@ public class GetDayWish {
 		int hours= now.getHour();
 		
 		
-		if (hours>=20 && hours <=6)
+		if (hours>=20 || hours <=6)
 		{
 			System.out.println("Good Night");
-		} else if(hours>=6 && hours<= 12)
+		} else if(hours>=6 || hours<= 12)
 		{
 			System.out.println("Good Morning");
-		} else if(hours>=12 && hours<= 16)
+		} else if(hours>=12 || hours<= 16)
 			
 		{
 			System.out.println("Good Afternoon");
-		} else if(hours>=16 && hours<= 20)
+		} else if(hours>=16 || hours<= 20)
 		{
 			System.out.println("Good Evening");
 		}
