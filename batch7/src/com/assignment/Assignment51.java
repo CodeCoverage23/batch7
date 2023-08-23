e93519dcbce4e45b2e6ad9ca3dc1b7926287dba2
Dell <Dell@DESKTOP-1L19HP1>
2023-08-23 22:20:11 +0530

    Assignment51

---
+++ batch7/src/com/assignment/Assignment51.java
@@ -0,0 +1,20 @@
+package com.assignment;
+
+public class Assignment51 {
+
+	public static void main(String[] args) {
+		int num=12;
+		if(num>0)
+		{
+			System.out.println("number is positive");
+			
+		}
+		else if(num<0);
+		{
+			System.out.println("number is negative");
+		// ODO Auto-generated method stub
+
+	}
+
+}
+}
