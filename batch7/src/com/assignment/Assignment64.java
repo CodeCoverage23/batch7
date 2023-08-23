1f3a75ebceae8a9f28f0040a3dcb0e50f789589b
Dell <Dell@DESKTOP-1L19HP1>
2023-08-23 22:42:29 +0530

    Assignment64

---
+++ batch7/src/com/assignment/Assignment64.java
@@ -0,0 +1,20 @@
+package com.assignment;
+import java.util.Scanner;
+
+public class Assignment64 {
+
+	public static void main(String[] args) {
+		int n,fact=1;
+		System.out.print("Enter a number");
+		Scanner r=new Scanner(System.in);
+		n=r.nextInt();
+		for(int i=1; i<=n; i++);
+		{
+			fact=fact*1;
+		// TODO Auto-generated method stub
+
+	  }
+		System.out.println("factorial"+fact);
+
+}
+}
