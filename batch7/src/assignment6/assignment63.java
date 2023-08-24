11b5e9ef5a2a70d98f9eb34c67bb92c373b13c60
Schaurase <139573310+Schaurase@users.noreply.github.com>
2023-08-24 20:01:06 +0530

    assignment6.3

---
+++ batch7/src/assignment6/assignment63.java
@@ -0,0 +1,19 @@
+package assignment6;
+
+import javax.swing.plaf.basic.BasicSplitPaneDivider;
+
+// .write a program  to print all even number from 50 to 75.
+public class assignment63 {
+	public static void main(String[] arge) {
+			System.out.println("List of even numbers from 50 to 75 : ");
+
+			for(int no=50;no<=75;no++) {
+				if (no%2==0) {
+					System.out.println(no);
+				}
+				
+			}
+
+}
+	
+}
