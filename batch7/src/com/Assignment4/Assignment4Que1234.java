f172628e4362856893abf87e4b97c74e8b2376a4
Admin <Admin@LAPTOP-27656PRT>
2023-08-27 20:41:46 +0530

    Assingment4Q5

--- batch7/src/com/Assignment4/Assignment4Que1234.java
+++ batch7/src/com/Assignment4/Assignment4Que1234.java
@@ -8,19 +8,23 @@ public class Assignment4Que1234 {
 		int c = a + b;
 		return c;
 	}
-public int sub(int k,int j ) {// Subtraction of two number
-	int l=j-k;
-	return l;
-}
-public int mult(int count1,int count2) { //multiplication of two number
-	int count3=count1*count2;
-	return count3;
-}
-public float div(int x,int y) { // dividation of two number
-	int z= x/y;
-	return z;
-	
-}
+
+	public int sub(int k, int j) {// Subtraction of two number
+		int l = j - k;
+		return l;
+	}
+
+	public int mult(int count1, int count2) { // multiplication of two number
+		int count3 = count1 * count2;
+		return count3;
+	}
+
+	public float div(int x, int y) { // dividation of two number
+		int z = x / y;
+		return z;
+
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Number :");
@@ -28,15 +32,13 @@ public float div(int x,int y) { // dividation of two number
 		int p = sc.nextInt();
 		Assignment4Que1234 m = new Assignment4Que1234();
 		int d = m.add(s, p);
-		System.out.println("addition " +d);
-		int l= m.sub(p, s);
-		System.out.println("subtraction" +l);
-		int count3= m.mult(s,p);
-		System.out.println("multiplication"+count3);
-		float z= m.div(p,s);
-		System.out.println("dividation"+z);
-		
-		
+		System.out.println("addition " + d);
+		int l = m.sub(p, s);
+		System.out.println("subtraction" + l);
+		int count3 = m.mult(s, p);
+		System.out.println("multiplication" + count3);
+		float z = m.div(p, s);
+		System.out.println("dividation" + z);
 
 	}
 
