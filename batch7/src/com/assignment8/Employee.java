182e28d66fe1f38d00957926b231b1772f3886f5
sirajuddin <nadafsiraj1096@gmail.com>
2023-09-08 12:04:07 -0400

    Assignment-8(Updated)

--- batch7/src/com/assignment8/Employee.java
+++ batch7/src/com/assignment8/Employee.java
@@ -1,11 +1,35 @@
 package com.assignment8;
 
-public class Employee 
+public class Employee
 {
 
-	String firstname, lastname, city,country;
-
+	String firstname, lastname, city;
+	static String country;
+     
 	long mobilenumber;
 	
 	
+	public Employee(String firstname, String lastname, String city, long mobilenumber)
+	{
+	
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.city = city;
+		this.mobilenumber=mobilenumber;
+		
+	
+	
+	}
+	public Employee(String country) 
+	{
+		this.country= country;
+	}
+	@Override
+	public String toString()
+	{
+
+
+	   
+		return  "student name:"+firstname +"\n"+"student Lastname:"+lastname+"\n"+"student city:"+city+"\n"+"student mobilnumber:"+mobilenumber+"\n"+"Student Country:"+country; 
+     }
 }
--- batch7/src/com/assignment8/EmployeeInformation.java
+++ batch7/src/com/assignment8/EmployeeInformation.java
@@ -1,4 +1,4 @@
-//1.1 Design the Employee class which contain
+////1.1 Design the Employee class which contain
 //firstName, lastName, city and mobileNumber and  country.
 //1.2 Design the class as EmployeeInformation which takes the input for multiple students from user and Design method public void getUserInput(), 
 //country is same for all the students.
@@ -11,23 +11,23 @@ import java.util.Scanner;
 
 public class EmployeeInformation
 {
-	
-	
-	static String country;
+	int num;//No of student data stored in num variable;
 
-	String firstname;
-	String lastname, city;
+	public Employee[] em;//Array of Employee 
+	String firstname, lastname, city;
+
+	static String country;
 	long mobilenumber;
 
-	int num;
-
+	//getUserInput()method  for getting input from user
 	public void getUserInput()
 	{
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter how many student data you want to get from user");
 		num = sc.nextInt();
-		for (int i = 0; i < num; i++)
+		em = new Employee[num];//No of Employee Objects Created
+		for (int i = 0; i < em.length; i++) 
 		{
 			System.out.println("Enter student firstname");
 			firstname = sc.next();
@@ -37,34 +37,31 @@ public class EmployeeInformation
 			city = sc.next();
 			System.out.println("Enter student mobilenumber");
 			mobilenumber = sc.nextLong();
-
+			em[i] = new Employee(firstname, lastname, city, mobilenumber);
 		}
-		System.out.println("Enter student country");
+		System.out.println("Enter student Country");
 		country = sc.next();
+		new Employee(country);
+	
 
 	}
-
-	public void getStudentInformation()
+    //Printing the multiple student information into getStudentInformation () method.
+	public void getStudentInformation() 
 	{
 
+		//Passing the input to getStudentInformation() method
 		getUserInput();
 
-		for (int i = 1; i <=num; i++)
-		{
-			System.out.println("<----Student"+i+"---->");
-			System.out.println("student name:" + firstname);
-			System.out.println("student Lastname:" + lastname);
-			System.out.println("student city:" + city);
-			System.out.println("student mobilnumber:" + mobilenumber);
-			System.out.println("student country:" + country);
+		for (int i = 0; i < em.length; i++) {
+
+			System.out.println(em[i]);
 		}
 
 	}
 
 	public static void main(String[] args) {
-
-		EmployeeInformation em = new EmployeeInformation();
-		em.getStudentInformation();
+		EmployeeInformation e = new EmployeeInformation();
+		e.getStudentInformation();
 
 	}
 
