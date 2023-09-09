141becf59539c59920c5a881bc388ae341e11a0e
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 13:29:06 +0530

    Assignment2q1

---
+++ batch7/src/com/Assignment/Assignment2q1.java
@@ -0,0 +1,27 @@
+package com.Assignment;
+// program which declare diffrent primitive datatypes.
+ 
+ 
+public class Assignment2q1 {
+	public static void main(String[] args) {
+		int a=28;
+		byte b=122;
+		float c=5;
+		char d='C';
+		boolean e=true;
+		double f=7832;
+		long g=56789;
+		short h=654;
+		
+		
+		System.out.println("a=" +a);
+		System.out.println("b"  +b);
+		System.out.println("c"  +c);
+		System.out.println("d"  +d);
+		System.out.println("e"  +e);
+		System.out.println("f"  +f);
+		System.out.println("g"  +g);
+		System.out.println("h"  +h);
+	}
+
+}
