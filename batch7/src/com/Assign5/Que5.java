cfb58c4e90f97824a2f4f6c6daf85521b4fe4c64
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 14:19:54 +0530

    Assignment5q5

---
+++ batch7/src/com/Assign5/Que5.java
@@ -0,0 +1,32 @@
+package com.Assign5;
+
+import java.util.Scanner;
+
+// write a program to check whether number is even or odd.
+
+
+public class Que5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		int num = sc.nextInt();
+		
+		if(num%2==0) {
+			System.out.println(num +" is an even number");
+			
+		}
+	
+		else {
+		System.out.println(num+" is an odd number");
+		
+		}
+		
+		
+	}
+	
+}	
+		
+	
+       	
+	
