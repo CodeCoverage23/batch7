bbf89cd3e020e90a156062b0105976731cba6517
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 14:22:31 +0530

    Assignment6q1

---
+++ batch7/src/com/Assign6/Que1.java
@@ -0,0 +1,20 @@
+package com.Assign6;
+
+// write a program to print 25 to 50 numbers using while loop.
+
+
+public class Que1 {
+
+public static void main(String[] args) {
+	    int num = 25;
+	
+	    while(num<=50) {
+		
+		System.out.println(num);
+		
+		num++;
+	}
+	   
+	   
+}
+}
