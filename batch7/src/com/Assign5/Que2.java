61c8e2307a8e7326780669205ec9f29d0caa4fb4
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 14:17:03 +0530

    Assignment5q2

---
+++ batch7/src/com/Assign5/Que2.java
@@ -0,0 +1,41 @@
+package com.Assign5;
+
+// write a program to check weather year is leap or not. 
+
+
+import java.util.Scanner;
+
+
+public class Que2 {
+public static void main(String[] args) {
+		
+  Scanner sc = new Scanner (System.in);
+  System.out.println("Enter the year to be checked for a leap year:");
+       
+   int year = sc.nextInt();
+     
+   if (year%4==0) {
+    	 
+ 	if (year%100==0) {
+    		 
+    		 
+    if (year%400==0) {
+    System.out.println(year +" is a leap year");
+    			 
+    			 
+    		 }
+    else {
+    System.out.println(year +" is not a leap year");
+    			 
+    		 }
+    	 }
+    else {
+    System.out.println(year + "is a leap year");
+    	 }
+    	 
+     }
+   else {
+	   System.out.println(year +"is not a leap year");
+   }
+	}
+}
