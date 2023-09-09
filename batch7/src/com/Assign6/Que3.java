3e661a96b5073901f08d4cc0bf72b94ad3fae637
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 14:23:57 +0530

    Assignment6q3

---
+++ batch7/src/com/Assign6/Que3.java
@@ -0,0 +1,18 @@
+package com.Assign6;
+// write a program to print all even number from 50 to 75.
+
+public class Que3 {
+	
+public static void main(String[] args) {
+		
+System.out.println("List of even numbers from 50 to 75:");
+
+       for (int no=50;no<=75;no++) {
+    	   if (no%2==0) {
+    		
+    		   System.out.println(no);
+    	      }
+       }
+	}
+
+}
