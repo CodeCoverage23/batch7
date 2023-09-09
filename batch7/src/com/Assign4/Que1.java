4e0a5609009c5e064a4a7c175a1b6e0456a2f050
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 14:07:36 +0530

    Assignment4q1

---
+++ batch7/src/com/Assign4/Que1.java
@@ -0,0 +1,28 @@
+package com.Assign4;
+
+//write the java program to design method for addition of two numbers 
+//which returns int results to that method and result should be print into main method.
+public class Que1 {
+	
+	public int add (int a,int b) {
+    return a+b;
+}
+
+	public static void main(String[] args) {
+		
+		Que1 s= new Que1();
+		
+		System.out.println(s.add(25, 15));
+	}
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
