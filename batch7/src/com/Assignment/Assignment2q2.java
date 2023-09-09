6c894d8af9c4360ff27cf996747e1db3199fc16f
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 13:37:14 +0530

    Assignment2q2

---
+++ batch7/src/com/Assignment/Assignment2q2.java
@@ -0,0 +1,12 @@
+ package com.Assignment;
+ // to print first character of your name
+ public class Assignment2q2 {
+	 
+	public static void main(String[] args) {
+		String str="Nilima";
+		char firstChar=str.charAt(0);
+		
+		System.out.println("First character of given string is: " + firstChar);
+	}
+
+}
