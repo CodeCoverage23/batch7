dc8e204ef5038b952bcd55ea9296b97d1b445b3a
LENOVO <LENOVO@LAPTOP-093DM0CO>
2023-09-09 13:50:16 +0530

    Assignment2q3

---
+++ batch7/src/com/Assignment/Assignment2q3.java
@@ -0,0 +1,18 @@
+package com.Assignment;
+
+public class Assignment2q3 {
+
+public static void main(String[] args) {
+	// que 3: to declare local variable as y
+	// que 4: to declare local variable as 70
+	
+	int a= 70;// declare local variable value as 70
+	char b='x';// declare local variable as y
+	
+	System.out.println("a=  " +a);
+	System.out.println("b=  "   +b);
+		
+		
+	}
+
+}
