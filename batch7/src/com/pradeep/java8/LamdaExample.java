8a2c261f270643b2458da1382e6ab4e59846f5e3
prade <prade@DESKTOP-9LC5689>
2023-09-14 08:17:32 +0530

    java8

---
+++ batch7/src/com/pradeep/java8/LamdaExample.java
@@ -0,0 +1,17 @@
+package com.pradeep.java8;
+@FunctionalInterface
+  interface Draw {
+	 public int circle( int a, int b);
+	 
+ }
+
+
+public class LamdaExample  {
+	
+	public static void main(String[] args) {
+		
+		Draw ex=(x,y)->x+y;
+			System.out.println(ex.circle(12,20));
+	}	
+
+}
