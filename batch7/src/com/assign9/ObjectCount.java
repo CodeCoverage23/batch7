f1629634ae660376bd16b49bb2e55c74a7a6af01
pratiksha <pratikshanaik3008@gmail.com>
2023-09-18 22:45:02 +0530

    assignment 9

---
+++ batch7/src/com/assign9/ObjectCount.java
@@ -0,0 +1,29 @@
+package com.assign9;
+
+public class ObjectCount {
+	static int count;
+	public ObjectCount() {
+		count++;
+	}
+	public static int getCountOfObject() {
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ObjectCount s1 = new ObjectCount();
+		ObjectCount s2 = new ObjectCount();
+		ObjectCount s3 = new ObjectCount();
+		System.out.println("object created until now : ");
+		System.out.println(getCountOfObject());
+		
+		ObjectCount s4 = new ObjectCount();
+		ObjectCount s5 = new ObjectCount();
+		ObjectCount s6 = new ObjectCount();
+		
+		System.out.println("object created at last : ");
+		System.out.println(getCountOfObject());
+
+	}
+
+}
